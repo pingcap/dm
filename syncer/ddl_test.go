4e8f000020c1cb5f2a27010939d622fd568c5154
GregoryIan <argregoryian@gmail.com>
2019-02-20 20:47:00 +0800

    add test cases

--- syncer/ddl_test.go
+++ syncer/ddl_test.go
@@ -21,7 +21,9 @@ import (
 	"github.com/pingcap/dm/dm/config"
 	parserpkg "github.com/pingcap/dm/pkg/parser"
 	"github.com/pingcap/dm/pkg/utils"
+	"github.com/pingcap/parser"
 	"github.com/pingcap/tidb-tools/pkg/filter"
+	router "github.com/pingcap/tidb-tools/pkg/table-router"
 )
 
 func (s *testSyncerSuite) TestTrimCtrlChars(c *C) {
@@ -121,7 +123,120 @@ func (s *testSyncerSuite) TestCommentQuote(c *C) {
 	c.Assert(sqls[0], Equals, expectedSQL)
 }
 
-func (s *testSyncerSuite) TestIgnoreDMLInQuery(c *C) {
+func (s *testSyncerSuite) TestresolveDDLSQL(c *C) {
+	// duplicate with pkg/parser
+	sqls := []string{
+		"create schema `s1`",
+		"create schema if not exists `s1`",
+		"drop schema `s1`",
+		"drop schema if exists `s1`",
+		"drop table `s1`.`t1`",
+		"drop table `s1`.`t1`, `s2`.`t2`",
+		"drop table `s1`.`t1`, `s2`.`t2`, `xx`",
+		"create table `s1`.`t1` (id int)",
+		"create table `t1` (id int)",
+		"create table `t1` like `t2`",
+		"create table `s1`.`t1` like `t2`",
+		"create table `t1` like `xx`.`t2`",
+		"truncate table `t1`",
+		"truncate table `s1`.`t1`",
+		"rename table `s1`.`t1` to `s2`.`t2`",
+		"rename table `t1` to `t2`, `s1`.`t1` to `t2`",
+		"drop index i1 on `s1`.`t1`",
+		"drop index i1 on `t1`",
+		"create index i1 on `t1`(`c1`)",
+		"create index i1 on `s1`.`t1`(`c1`)",
+		"alter table `t1` add column c1 int, drop column c2",
+		"alter table `s1`.`t1` add column c1 int, rename to `t2`, drop column c2",
+		"alter table `s1`.`t1` add column c1 int, rename to `xx`.`t2`, drop column c2",
+	}
+
+	expectedSQLs := [][]string{
+		{"CREATE DATABASE IF NOT EXISTS `s1`"},
+		{"CREATE DATABASE IF NOT EXISTS `s1`"},
+		{"DROP DATABASE IF EXISTS `s1`"},
+		{"DROP DATABASE IF EXISTS `s1`"},
+		{"DROP TABLE IF EXISTS `s1`.`t1`"},
+		{"DROP TABLE IF EXISTS `s1`.`t1`", "DROP TABLE IF EXISTS `s2`.`t2`"},
+		{"DROP TABLE IF EXISTS `s1`.`t1`", "DROP TABLE IF EXISTS `s2`.`t2`", "DROP TABLE IF EXISTS `test`.`xx`"},
+		{"CREATE TABLE IF NOT EXISTS `s1`.`t1` (`id` INT)"},
+		{"CREATE TABLE IF NOT EXISTS `test`.`t1` (`id` INT)"},
+		{"CREATE TABLE IF NOT EXISTS `test`.`t1` LIKE `test`.`t2`"},
+		{"CREATE TABLE IF NOT EXISTS `s1`.`t1` LIKE `test`.`t2`"},
+		{"CREATE TABLE IF NOT EXISTS `test`.`t1` LIKE `xx`.`t2`"},
+		{"TRUNCATE TABLE `test`.`t1`"},
+		{"TRUNCATE TABLE `s1`.`t1`"},
+		{"RENAME TABLE `s1`.`t1` TO `s2`.`t2`"},
+		{"RENAME TABLE `test`.`t1` TO `test`.`t2`", "RENAME TABLE `s1`.`t1` TO `test`.`t2`"},
+		{"DROP INDEX IF EXISTS `i1` ON `s1`.`t1`"},
+		{"DROP INDEX IF EXISTS `i1` ON `test`.`t1`"},
+		{"CREATE INDEX `i1` ON `test`.`t1` (`c1`)"},
+		{"CREATE INDEX `i1` ON `s1`.`t1` (`c1`)"},
+		{"ALTER TABLE `test`.`t1` ADD COLUMN `c1` INT", "ALTER TABLE `test`.`t1` DROP COLUMN `c2`"},
+		{"ALTER TABLE `s1`.`t1` ADD COLUMN `c1` INT", "ALTER TABLE `s1`.`t1` RENAME AS `test`.`t2`", "ALTER TABLE `test`.`t2` DROP COLUMN `c2`"},
+		{"ALTER TABLE `s1`.`t1` ADD COLUMN `c1` INT", "ALTER TABLE `s1`.`t1` RENAME AS `xx`.`t2`", "ALTER TABLE `xx`.`t2` DROP COLUMN `c2`"},
+	}
+
+	targetSQLs := [][]string{
+		{"CREATE DATABASE IF NOT EXISTS `xs1`"},
+		{"CREATE DATABASE IF NOT EXISTS `xs1`"},
+		{"DROP DATABASE IF EXISTS `xs1`"},
+		{"DROP DATABASE IF EXISTS `xs1`"},
+		{"DROP TABLE IF EXISTS `xs1`.`t1`"},
+		{"DROP TABLE IF EXISTS `xs1`.`t1`", ""},
+		{"DROP TABLE IF EXISTS `xs1`.`t1`", "", ""},
+		{"CREATE TABLE IF NOT EXISTS `xs1`.`t1` (`id` INT)"},
+		{""},
+		{""},
+		{""},
+		{""},
+		{""},
+		{"TRUNCATE TABLE `xs1`.`t1`"},
+		{""},
+		{"", ""},
+		{"DROP INDEX IF EXISTS `i1` ON `xs1`.`t1`"},
+		{""},
+		{""},
+		{"CREATE INDEX `i1` ON `xs1`.`t1` (`c1`)"},
+		{"", ""},
+		{"ALTER TABLE `xs1`.`t1` ADD COLUMN `c1` INT", "", ""},
+		{"ALTER TABLE `xs1`.`t1` ADD COLUMN `c1` INT", "", ""},
+	}
+
+	p := parser.New()
+	cfg := &config.SubTaskConfig{
+		BWList: &filter.Rules{
+			DoDBs: []string{"s1"},
+		},
+	}
+	syncer := NewSyncer(cfg)
+
+	syncer.tableRouter = router.NewTableRouter(false, []*router.TableRule{
+		{
+			SchemaPattern: "s1",
+			TargetPattern: "xs1",
+		},
+	})
+
+	for i, sql := range sqls {
+		result, err := syncer.parseDDLSQL(sql, p, "test")
+		c.Assert(err, IsNil)
+		c.Assert(result.ignore, IsFalse)
+		c.Assert(result.isDDL, IsTrue)
+
+		statements, _, err := syncer.resolveDDLSQL(p, result.stmt, "test")
+		c.Assert(err, IsNil)
+		c.Assert(statements, DeepEquals, expectedSQLs[i])
+
+		for j, statement := range statements {
+			s, _, _, err := syncer.handleDDL(p, "test", statement)
+			c.Assert(err, IsNil)
+			c.Assert(s, Equals, targetSQLs[i][j])
+		}
+	}
+}
+
+func (s *testSyncerSuite) TestParseDDLSQL(c *C) {
 	cases := []struct {
 		sql      string
 		schema   string
@@ -129,6 +244,20 @@ func (s *testSyncerSuite) TestIgnoreDMLInQuery(c *C) {
 		isDDL    bool
 		hasError bool
 	}{
+		{
+			sql:      "FLUSH",
+			schema:   "",
+			ignore:   true,
+			isDDL:    false,
+			hasError: false,
+		},
+		{
+			sql:      "BEGIN",
+			schema:   "",
+			ignore:   false,
+			isDDL:    false,
+			hasError: false,
+		},
 		{
 			sql:      "CREATE TABLE do_db.do_table (c1 INT)",
 			schema:   "",
