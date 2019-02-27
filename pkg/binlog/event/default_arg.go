cd9d3632b812f0320a8a694b8d381693b5696dcf
csuzhangxc <csuzhangxc@gmail.com>
2019-02-27 16:28:51 +0800

    event: generate events for CREATE/DROP DATABASE

---
+++ pkg/binlog/event/default_arg.go
@@ -0,0 +1,9 @@
+package event
+
+var (
+	defaultHeaderFlags   uint16 = 0x01 // LOG_EVENT_BINLOG_IN_USE_F
+	defaultSlaveProxyID  uint32 = 2
+	defaultExecutionTime uint32
+	defaultErrorCode     uint16
+	defaultStatusVars    []byte
+)
---
+++ pkg/binlog/event/schema.go
@@ -0,0 +1,33 @@
+package event
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/pingcap/errors"
+	"github.com/siddontang/go-mysql/replication"
+)
+
+// GenCreateDatabase generates binlog events for `CREATE DATABASE`.
+func GenCreateDatabase(serverID uint32, latestPos uint32, schema string) ([]*replication.BinlogEvent, []byte, error) {
+	return genDatabaseEvents(serverID, latestPos, schema, "CREATE DATABASE `%s`")
+}
+
+// GenDropDatabase generates binlog events for `DROP DATABASE`.
+func GenDropDatabase(serverID uint32, latestPos uint32, schema string) ([]*replication.BinlogEvent, []byte, error) {
+	return genDatabaseEvents(serverID, latestPos, schema, "DROP DATABASE `%s`")
+}
+
+func genDatabaseEvents(serverID uint32, latestPos uint32, schema string, template string) ([]*replication.BinlogEvent, []byte, error) {
+	header := &replication.EventHeader{
+		Timestamp: uint32(time.Now().Unix()),
+		ServerID:  serverID,
+		Flags:     defaultHeaderFlags,
+	}
+	query := fmt.Sprintf(template, schema)
+	ev, err := GenQueryEvent(header, latestPos, defaultSlaveProxyID, defaultExecutionTime, defaultErrorCode, defaultStatusVars, []byte(schema), []byte(query))
+	if err != nil {
+		return nil, nil, errors.Annotatef(err, "generate QueryEvent for schema %s", schema)
+	}
+	return []*replication.BinlogEvent{ev}, ev.RawData, nil
+}
