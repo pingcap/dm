8318a7b0f7c7de3acce663f79ae65fd5f563b4fa
amyangfei <amyangfei@gmail.com>
2019-03-15 17:33:02 +0800

    test: add online ddl test

---
+++ tests/online_ddl/data/db2.increment.sql
@@ -0,0 +1,14 @@
+use online_ddl;
+delete from t3 where name = 'Santa Sofía de la Piedad';
+alter table t2 add column age int;
+update t2 set uid = uid + 10000;
+alter table t3 add column age int;
+update t3 set age = 1;
+alter table t2 add column info_json json GENERATED ALWAYS AS (`info`) VIRTUAL;
+update t3 set age = age + 10;
+alter table t3 add column info_json json GENERATED ALWAYS AS (`info`) VIRTUAL;
+alter table t3 add column id_gen int as (uid + 1);
+alter table t2 add column id_gen int as (uid + 1);
+alter table t2 add unique (id_gen);
+alter table t3 add unique (id_gen);
+update t2 set age = age + 10;
---
+++ tests/online_ddl/run.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -eu
+
+cur=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $cur/../_utils/test_prepare
+WORK_DIR=$TEST_DIR/$TEST_NAME
+
+function run() {
+    run_sql_file $cur/data/db1.prepare.sql $DB1_PORT
+    check_contains 'Query OK, 2 rows affected'
+    run_sql_file $cur/data/db2.prepare.sql $DB2_PORT
+    check_contains 'Query OK, 3 rows affected'
+
+    run_dm_worker $WORK_DIR/worker1 $WORKER1_PORT $cur/conf/dm-worker1.toml
+    run_dm_worker $WORK_DIR/worker2 $WORKER2_PORT $cur/conf/dm-worker2.toml
+    run_dm_master $WORK_DIR/master $MASTER_PORT $cur/conf/dm-master.toml
+
+    check_port_alive $MASTER_PORT
+    check_port_alive $WORKER1_PORT
+    check_port_alive $WORKER2_PORT
+
+    cd $cur && GO111MODULE=on go build -o bin/dmctl && cd -
+    # start DM task only
+    $cur/bin/dmctl "$cur/conf/dm-task.yaml"
+
+    # TODO: check sharding partition id
+    # use sync_diff_inspector to check full dump loader
+    check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
+
+    run_sql_file_online_ddl $cur/data/db1.increment.sql $DB1_PORT $TEST_NAME gh-ost
+    run_sql_file_online_ddl $cur/data/db2.increment.sql $DB2_PORT $TEST_NAME gh-ost
+
+    # TODO: check sharding partition id
+    # use sync_diff_inspector to check data now!
+    check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
+}
+
+cleanup1 $*
+# also cleanup dm processes in case of last run failed
+cleanup2 $*
+run $*
+cleanup2 $*
+
+wait_process_exit dm-master.test
+wait_process_exit dm-worker.test
+
+echo "[$(date)] <<<<<< test case $TEST_NAME success! >>>>>>"
