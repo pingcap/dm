ae28bc0942eae20c6ed978ece3cedd9d20b6413f
GregoryIan <argregoryian@gmail.com>
2019-03-25 13:25:04 +0800

    *: fix ci

--- tests/all_mode/run.sh
+++ tests/all_mode/run.sh
@@ -31,8 +31,8 @@ function run() {
     wait_process_exit dm-worker.test
 
     # restart dm-worker
-    restart_dm_worker $WORK_DIR/worker1 $WORKER1_PORT $cur/conf/dm-worker1.toml
-    restart_dm_worker $WORK_DIR/worker2 $WORKER2_PORT $cur/conf/dm-worker2.toml
+    run_dm_worker $WORK_DIR/worker1 $WORKER1_PORT $cur/conf/dm-worker1.toml
+    run_dm_worker $WORK_DIR/worker2 $WORKER2_PORT $cur/conf/dm-worker2.toml
 
     run_sql_file $cur/data/db1.increment.sql $MYSQL_HOST1 $MYSQL_PORT1
     run_sql_file $cur/data/db2.increment.sql $MYSQL_HOST2 $MYSQL_PORT2
