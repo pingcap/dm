efe18d51000367b8407b6c53aed46143de0dbcba
amyangfei <amyangfei@gmail.com>
2019-04-24 10:13:43 +0800

    address comment, add checksum comparision to unchanged rows

--- tests/sharding/run.sh
+++ tests/sharding/run.sh
@@ -6,6 +6,16 @@ cur=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $cur/../_utils/test_prepare
 WORK_DIR=$TEST_DIR/$TEST_NAME
 
+function checksum() {
+    read -d '' sql << EOF
+SELECT BIT_XOR(CAST(CRC32(CONCAT_WS(',', uid, name, info, age, id_gen,
+    CONCAT(ISNULL(uid), ISNULL(name), ISNULL(info), ISNULL(age), ISNULL(id_gen)))) AS UNSIGNED)) AS checksum
+    FROM db_target.t_target WHERE (uid > 70000);
+EOF
+    run_sql "$sql" $TIDB_PORT
+    echo $(tail -n 1 "$TEST_DIR/sql_res.$TEST_NAME.txt")
+}
+
 function run() {
     run_sql_file $cur/data/db1.prepare.sql $MYSQL_HOST1 $MYSQL_PORT1
     check_contains 'Query OK, 2 rows affected'
@@ -42,6 +52,8 @@ function run() {
     printf "\n[[table-config.source-tables]]\ninstance-id = \"source-2\"\nschema = \"sharding2\"\ntable  = \"~t.*\"" >> $WORK_DIR/diff_config.toml
     check_sync_diff $WORK_DIR $WORK_DIR/diff_config.toml
 
+    old_checksum=$(checksum)
+
     # test drop table, drop database, truncate table in sharding mode
     run_sql_file $cur/data/db1.increment3.sql $MYSQL_HOST1 $MYSQL_PORT1
     run_sql_file $cur/data/db2.increment3.sql $MYSQL_HOST2 $MYSQL_PORT2
@@ -49,6 +61,10 @@ function run() {
     printf "\n[[table-config.source-tables]]\ninstance-id = \"source-1\"\nschema = \"sharding2\"\ntable  = \"~t.*\"" >> $WORK_DIR/diff_config.toml
     sed -i "s/^# range-placeholder/range = \"uid < 70000\"/g" $WORK_DIR/diff_config.toml
     check_sync_diff $WORK_DIR $WORK_DIR/diff_config.toml
+
+    new_checksum=$(checksum)
+    echo "checksum before drop/truncate: $old_checksum, checksum after drop/truncate: $new_checksum"
+    [ "$old_checksum" == "$new_checksum" ]
 }
 
 cleanup1 db_target
