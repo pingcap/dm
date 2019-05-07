3dd17f4e0554acd95297bea8c1974c8a44921a77
amyangfei <amyangfei@gmail.com>
2019-05-07 19:20:56 +0800

    add query-status test

--- tests/dmctl_basic/run.sh
+++ tests/dmctl_basic/run.sh
@@ -10,6 +10,10 @@ function run() {
     run_sql_file $cur/data/db1.prepare.sql $MYSQL_HOST1 $MYSQL_PORT1
     run_sql_file $cur/data/db2.prepare.sql $MYSQL_HOST2 $MYSQL_PORT2
 
+    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+        "query-status -w worker-x task-y" \
+        "can not query task-y task's status(in workers \[worker-x\]):" 1
+
     run_dm_worker $WORK_DIR/worker1 $WORKER1_PORT $cur/conf/dm-worker1.toml
     run_dm_worker $WORK_DIR/worker2 $WORKER2_PORT $cur/conf/dm-worker2.toml
     run_dm_master $WORK_DIR/master $MASTER_PORT $cur/conf/dm-master.toml
@@ -18,8 +22,23 @@ function run() {
     check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER2_PORT
     check_rpc_alive $cur/../bin/check_master_online 127.0.0.1:$MASTER_PORT
 
+    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+        "query-status wrong_args_count more_than_one" \
+        "query-status \[-w worker ...\] \[task_name\]" 1
+
+    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+        "query-status" \
+        "\"result\": true" 3 \
+        "\"msg\": \"no sub task started\"" 2
+
     # start DM task only
     dmctl_start_task
+
+    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+        "query-status" \
+        "\"result\": true" 3 \
+        "\"unit\": \"Sync\"" 2 \
+        "\"stage\": \"Running\"" 4
 
     check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
 
