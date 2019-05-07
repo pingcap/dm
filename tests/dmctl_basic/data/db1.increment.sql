5fba5333d9b3f70c3ddd53dfc83adf7d4739d8b8
amyangfei <amyangfei@gmail.com>
2019-05-07 17:49:19 +0800

    add new test case for dmctl test

---
+++ tests/dmctl_basic/data/db1.increment.sql
@@ -0,0 +1,18 @@
+UPDATE `dmctl`.`t_2` SET `c` = 'lnVaSBHOTnAatvUmHZIZ' WHERE `id` = 6;
+INSERT INTO `dmctl`.`t_1` (`b`,`c`,`d`,`id`) VALUES (1207302373,'slOcgVAMoRUmxBpRdUHe','h',10);
+UPDATE `dmctl`.`t_1` SET `c` = 'uvSLJRmOHYLDDMM' WHERE `id` = 9;
+DELETE FROM `dmctl`.`t_1` WHERE `id` = 2;
+INSERT INTO `dmctl`.`t_1` (`b`,`c`,`d`,`id`) VALUES (617131993,'Klf','dZLD',11);
+UPDATE `dmctl`.`t_2` SET `d` = 'k' WHERE `id` = 3;
+UPDATE `dmctl`.`t_1` SET `d` = 'Nlh' WHERE `id` = 4;
+INSERT INTO `dmctl`.`t_1` (`b`,`c`,`d`,`id`) VALUES (241968486,'LT','JRvYDYFX',12);
+UPDATE `dmctl`.`t_1` SET `c` = 'BZbKSuZKitYG' WHERE `id` = 7;
+DELETE FROM `dmctl`.`t_2` WHERE `id` = 3;
+INSERT INTO `dmctl`.`t_2` (`b`,`c`,`d`,`id`) VALUES (207449096,'upYYtysTjztxkYGprAHD','drBN',11);
+UPDATE `dmctl`.`t_2` SET `c` = 'AVKxHwiUZHXXKqODQoE' WHERE `id` = 8;
+UPDATE `dmctl`.`t_2` SET `d` = 'eLmCZwCA' WHERE `id` = 9;
+INSERT INTO `dmctl`.`t_2` (`b`,`c`,`d`,`id`) VALUES (683005299,'lsATr','mNOpyfu',12);
+UPDATE `dmctl`.`t_2` SET `d` = 'uVNNR' WHERE `id` = 6;
+DELETE FROM `dmctl`.`t_2` WHERE `id` = 5;
+INSERT INTO `dmctl`.`t_1` (`b`,`c`,`d`,`id`) VALUES (373155634,'uoxJTwvCSEuaRbWGq','xzCteQqZwU',13);
+UPDATE `dmctl`.`t_1` SET `d` = 'GzYx' WHERE `id` = 6;
---
+++ tests/dmctl_basic/run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -eu
+
+cur=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $cur/../_utils/test_prepare
+WORK_DIR=$TEST_DIR/$TEST_NAME
+
+function run() {
+    run_sql_file $cur/data/db1.prepare.sql $MYSQL_HOST1 $MYSQL_PORT1
+    run_sql_file $cur/data/db2.prepare.sql $MYSQL_HOST2 $MYSQL_PORT2
+
+    run_dm_worker $WORK_DIR/worker1 $WORKER1_PORT $cur/conf/dm-worker1.toml
+    run_dm_worker $WORK_DIR/worker2 $WORKER2_PORT $cur/conf/dm-worker2.toml
+    run_dm_master $WORK_DIR/master $MASTER_PORT $cur/conf/dm-master.toml
+
+    check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER1_PORT
+    check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER2_PORT
+    check_rpc_alive $cur/../bin/check_master_online 127.0.0.1:$MASTER_PORT
+
+    # start DM task only
+    dmctl_start_task
+
+    check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
+
+    run_sql_file $cur/data/db1.increment.sql $MYSQL_HOST1 $MYSQL_PORT1
+    run_sql_file $cur/data/db2.increment.sql $MYSQL_HOST2 $MYSQL_PORT2
+
+    # TODO: check sharding partition id
+    # use sync_diff_inspector to check data now!
+    check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
+}
+
+cleanup1 dmctl
+# also cleanup dm processes in case of last run failed
+cleanup2 $*
+run $*
+cleanup2 $*
+
+wait_process_exit dm-master.test
+wait_process_exit dm-worker.test
+
+echo "[$(date)] <<<<<< test case $TEST_NAME success! >>>>>>"
