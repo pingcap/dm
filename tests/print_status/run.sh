90c1387b42ffe479e7c757e6e070d3202f836a73
amyangfei <amyangfei@gmail.com>
2019-05-13 13:31:47 +0800

    *: fix start up and check alive sequence

--- tests/print_status/run.sh
+++ tests/print_status/run.sh
@@ -21,9 +21,8 @@ function run() {
     export GO_FAILPOINTS="$(join_string \; ${inject_points[@]})"
 
     run_dm_worker $WORK_DIR/worker1 $WORKER1_PORT $cur/conf/dm-worker1.toml
-    run_dm_master $WORK_DIR/master $MASTER_PORT $cur/conf/dm-master.toml
-
     check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER1_PORT
+    run_dm_master $WORK_DIR/master $MASTER_PORT $cur/conf/dm-master.toml
     check_rpc_alive $cur/../bin/check_master_online 127.0.0.1:$MASTER_PORT
 
     # start DM task only
