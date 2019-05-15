578d8120c1a54df160279fb5b39e9b7f64175356
GregoryIan <argregoryian@gmail.com>
2019-05-15 21:10:55 +0800

    *: add some ut for worker

--- dm/worker/worker_test.go
+++ dm/worker/worker_test.go
@@ -17,17 +17,21 @@ import (
 	"testing"
 
 	. "github.com/pingcap/check"
+	"github.com/pingcap/dm/dm/config"
+	"github.com/pingcap/dm/dm/pb"
 )
 
 func TestWorker(t *testing.T) {
 	TestingT(t)
 }
 
+var emptyWorkerStatusInfoJSONLength = 25
+
 type testWorker struct{}
 
 var _ = Suite(&testWorker{})
 
-func (t *testWorker) TestNewWorker(c *C) {
+func (t *testWorker) TestWorker(c *C) {
 	cfg := NewConfig()
 	c.Assert(cfg.Parse([]string{"-config=./dm-worker.toml"}), IsNil)
 
@@ -44,6 +48,63 @@ func (t *testWorker) TestNewWorker(c *C) {
 	c.Assert(err, ErrorMatches, "init error")
 
 	NewRelayHolder = NewDummyRelayHolder
-	_, err = NewWorker(cfg)
+	w, err := NewWorker(cfg)
 	c.Assert(err, IsNil)
+	c.Assert(w.StatusJSON(""), HasLen, emptyWorkerStatusInfoJSONLength)
+	c.Assert(w.closed.Get(), Equals, closedFalse)
+
+	// start task
+	c.Assert(w.meta.LoadTaskMeta(), HasLen, 0)
+	c.Assert(w.meta.PeekLog(), IsNil)
+
+	id1, err := w.StartSubTask(&config.SubTaskConfig{
+		Name: "testStartTask",
+	})
+	c.Assert(err, IsNil)
+	c.Assert(w.meta.PeekLog(), NotNil)
+	c.Assert(w.meta.PeekLog().Id, Equals, id1)
+	c.Assert(id1, Equals, int64(1))
+
+	id2, err := w.UpdateSubTask(&config.SubTaskConfig{
+		Name: "testStartTask",
+	})
+	c.Assert(err, IsNil)
+	c.Assert(id2, Equals, int64(2))
+	c.Assert(w.meta.PeekLog(), NotNil)
+	c.Assert(w.meta.PeekLog().Id, Equals, id1)
+
+	log2, err := w.meta.GetTaskLog(id2)
+	c.Assert(err, IsNil)
+	c.Assert(log2, NotNil)
+
+	id3, err := w.OperateSubTask("testSubTask", pb.TaskOp_Stop)
+	c.Assert(err, IsNil)
+	c.Assert(id3, Equals, int64(3))
+	c.Assert(w.meta.PeekLog(), NotNil)
+	c.Assert(w.meta.PeekLog().Id, Equals, id1)
+
+	log3, err := w.meta.GetTaskLog(id3)
+	c.Assert(err, IsNil)
+	c.Assert(log3, NotNil)
+
+	// close twice
+	w.Close()
+	c.Assert(w.closed.Get(), Equals, closedTrue)
+	c.Assert(w.subTasks, IsNil)
+	w.Close()
+	c.Assert(w.closed.Get(), Equals, closedTrue)
+	c.Assert(w.subTasks, IsNil)
+
+	_, err = w.StartSubTask(&config.SubTaskConfig{
+		Name: "testStartTask",
+	})
+	c.Assert(err, ErrorMatches, "worker already closed.*")
+
+	_, err = w.UpdateSubTask(&config.SubTaskConfig{
+		Name: "testStartTask",
+	})
+	c.Assert(err, ErrorMatches, "worker already closed.*")
+
+	_, err = w.OperateSubTask("testSubTask", pb.TaskOp_Stop)
+	c.Assert(err, ErrorMatches, "worker already closed.*")
 }
