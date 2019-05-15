6edd0a59cfd70b93511d48a5b0ea2b3901d061b9
WangXiangUSTC <xiang13225080@163.com>
2019-05-15 15:34:56 +0800

    add test for job

---
+++ syncer/job_test.go
@@ -0,0 +1,98 @@
+// Copyright 2019 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package syncer
+
+import (
+	. "github.com/pingcap/check"
+
+	"github.com/siddontang/go-mysql/mysql"
+)
+
+var _ = Suite(&testJobSuite{})
+
+type testJobSuite struct {}
+
+func (t *testJobSuite) TestJobTypeString(c *C) {
+	testCases := []struct {
+		tp opType
+		str string
+	} {
+		{
+			null,
+			"",
+		}, {
+			insert,
+			"insert",
+		}, {
+			update,
+			"update",
+		}, {
+			del,
+			"delete",
+		}, {
+			ddl,
+			"ddl",
+		}, {
+			xid,
+			"xid",
+		}, {
+			flush,
+			"flush",
+		}, {
+			skip,
+			"skip",
+		}, {
+			rotate,
+			"rotate",
+		},
+	}
+
+	for _, testCase := range testCases {
+		tpStr := testCase.tp.String()
+		c.Assert(tpStr, Equals, testCase.str)
+	}
+}
+
+func (t *testJobSuite) TestJob(c *C) {
+	testCases := []struct {
+		job *job
+		jobStr string
+	} {
+		{
+			newJob(insert, "test", "t1", "test", "t1", "insert into test.t1 values(?)", []interface{}{1}, 	"1", mysql.Position{}, mysql.Position{}, nil, ""),
+			"tp: insert, sql: insert into test.t1 values(?), args: [1], key: 1, ddls: [], last_pos: (, 0), current_pos: (, 0), gtid:<nil>",
+		}, {
+			newDDLJob(nil, []string{"create database test",}, mysql.Position{}, mysql.Position{}, nil, nil, ""),
+			"tp: ddl, sql: , args: [], key: , ddls: [create database test], last_pos: (, 0), current_pos: (, 0), gtid:<nil>",
+		}, {
+			newXIDJob(mysql.Position{}, mysql.Position{}, nil, ""),
+			"tp: xid, sql: , args: [], key: , ddls: [], last_pos: (, 0), current_pos: (, 0), gtid:<nil>",
+		}, {
+			newFlushJob(),
+			"tp: flush, sql: , args: [], key: , ddls: [], last_pos: (, 0), current_pos: (, 0), gtid:<nil>",
+		}, {
+			newSkipJob(mysql.Position{}, nil),
+			"tp: skip, sql: , args: [], key: , ddls: [], last_pos: (, 0), current_pos: (, 0), gtid:<nil>",
+		},
+	}
+
+	for _, testCase := range testCases {
+		c.Assert(testCase.job.String(), Equals, testCase.jobStr)
+	}
+}
+
+func (s *testJobSuite) TestqueueBucketName(c *C) {
+	name := queueBucketName(9)
+	c.Assert(name, Equals, "q_1")
+}
