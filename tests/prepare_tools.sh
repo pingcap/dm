8fa35d6acd1d2443a9f0d558b63f753f6183a68d
amyangfei <amyangfei@gmail.com>
2019-05-31 13:07:38 +0800

    tests: support run integration tests in parallel (#163)

---
+++ tests/prepare_tools.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CUR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+mkdir -p $CUR/bin
+cd $CUR
+for file in "_dmctl_tools"/*; do
+    bin_name=$(echo $file|awk -F"/" '{print $(NF)}'|awk -F"." '{print $1}')
+    GO111MODULE=on go build -o bin/$bin_name $file
+done
+cd -
