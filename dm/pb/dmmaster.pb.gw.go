d98bab5fcc10582e21fd60e62b0e56955c73d213
WangXiangUSTC <xiang13225080@163.com>
2019-08-02 12:14:23 +0800

    update generate pb file script

--- dm/pb/dmmaster.pb.gw.go
+++ dm/pb/dmmaster.pb.gw.go
@@ -80,6 +80,44 @@ func request_Master_OperateTask_0(ctx context.Context, marshaler runtime.Marshal
 
 }
 
+var (
+	filter_Master_QueryStatus_0 = &utilities.DoubleArray{Encoding: map[string]int{"name": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+)
+
+func request_Master_QueryStatus_0(ctx context.Context, marshaler runtime.Marshaler, client MasterClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq QueryStatusListRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["name"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "name")
+	}
+
+	protoReq.Name, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "name", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Master_QueryStatus_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.QueryStatus(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterMasterHandlerFromEndpoint is same as RegisterMasterHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterMasterHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -158,6 +196,26 @@ func RegisterMasterHandlerClient(ctx context.Context, mux *runtime.ServeMux, cli
 
 	})
 
+	mux.Handle("GET", pattern_Master_QueryStatus_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Master_QueryStatus_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Master_QueryStatus_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -165,10 +223,14 @@ var (
 	pattern_Master_StartTask_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"apis", "alpha", "tasks"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_Master_OperateTask_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"apis", "alpha", "tasks", "name"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_Master_QueryStatus_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"apis", "alpha", "status", "name"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
 	forward_Master_StartTask_0 = runtime.ForwardResponseMessage
 
 	forward_Master_OperateTask_0 = runtime.ForwardResponseMessage
+
+	forward_Master_QueryStatus_0 = runtime.ForwardResponseMessage
 )
--- generate-dm.sh
+++ generate-dm.sh
@@ -10,17 +10,27 @@ if [ ! -d $GOGO_ROOT ]; then
 		exit 1
 fi
 
-GATEWAY_ROOT=${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
-if [ ! -d $GOGO_ROOT ]; then
+GATEWAY_ROOT=${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway
+if [ ! -d $GATEWAY_ROOT ]; then
 	echo "please use the following command to get grpc-gateway."
 	echo "go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
 	exit 1
 fi
 
+GOOGLE_API_ROOT=${GOPATH}/src/github.com/googleapis/googleapis
+if [ ! -d $GOOGLE_API_ROOT ]; then
+	echo "please use the following command to get google apis."
+	echo "go get -u github.com/googleapis/googleapis/google/api"
+	exit 1
+fi
+
+cp -r ${GOPATH}/src/github.com/googleapis/googleapis/google ./
+
 protoc -I.:${GOGO_ROOT}:${GOGO_ROOT}/protobuf --gogofaster_out=plugins=grpc:../pb/ *.proto
 
 protoc -I.:${GOGO_ROOT}:${GOGO_ROOT}/protobuf --grpc-gateway_out=logtostderr=true:../pb/ dmmaster.proto
 
+rm -r ./google
 
 cd ../pb
 sed -i.bak -E 's/import _ \"gogoproto\"//g' *.pb.go
