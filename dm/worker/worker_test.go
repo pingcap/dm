076252d6c5066fe029f257b35b7baa22e32b9eb3
amyangfei <amyangfei@gmail.com>
2019-08-05 17:47:35 +0800

    worker: fix panic in handle task

--- dm/worker/worker_test.go
+++ dm/worker/worker_test.go
@@ -14,10 +14,15 @@
 package worker
 
 import (
+	"sync"
+	"time"
+
 	. "github.com/pingcap/check"
+	"github.com/pingcap/failpoint"
 
 	"github.com/pingcap/dm/dm/config"
 	"github.com/pingcap/dm/dm/pb"
+	"github.com/pingcap/dm/pkg/utils"
 )
 
 var emptyWorkerStatusInfoJSONLength = 25
@@ -64,4 +69,48 @@ func (t *testServer) testWorker(c *C) {
 
 	_, err = w.OperateSubTask("testSubTask", pb.TaskOp_Stop)
 	c.Assert(err, ErrorMatches, "worker already closed.*")
+}
+
+func (t *testServer) TestHandleTask(c *C) {
+	var (
+		wg       sync.WaitGroup
+		taskName = "test"
+	)
+
+	NewRelayHolder = NewDummyRelayHolder
+	dir := c.MkDir()
+	cfg := NewConfig()
+	c.Assert(cfg.Parse([]string{"-config=./dm-worker.toml"}), IsNil)
+	cfg.RelayDir = dir
+	cfg.MetaDir = dir
+	w, err := NewWorker(cfg)
+	c.Assert(err, IsNil)
+
+	tasks := []*pb.TaskMeta{
+		{Op: pb.TaskOp_Stop, Name: taskName, Stage: pb.Stage_New},
+		{Op: pb.TaskOp_Pause, Name: taskName, Stage: pb.Stage_New},
+		{Op: pb.TaskOp_Resume, Name: taskName, Stage: pb.Stage_New},
+	}
+	for _, task := range tasks {
+		_, err := w.meta.AppendOperation(task)
+		c.Assert(err, IsNil)
+	}
+	c.Assert(len(w.meta.logs), Equals, len(tasks))
+
+	c.Assert(failpoint.Enable("github.com/pingcap/dm/dm/worker/handleTaskInternal", `return(10)`), IsNil)
+	defer failpoint.Disable("github.com/pingcap/dm/dm/worker/handleTaskInternal")
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		w.handleTask()
+	}()
+
+	c.Assert(utils.WaitSomething(5, 10*time.Millisecond, func() bool {
+		w.meta.Lock()
+		defer w.meta.Unlock()
+		return len(w.meta.logs) == 0
+	}), IsTrue)
+
+	w.Close()
+	wg.Wait()
 }
