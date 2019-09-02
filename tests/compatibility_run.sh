bb30fcb299e0ef06678f1c5d7a48e8580708352d
WangXiangUSTC <xiang13225080@163.com>
2019-09-02 15:04:59 +0800

    remove useless code

--- tests/compatibility_run.sh
+++ tests/compatibility_run.sh
@@ -6,15 +6,6 @@ TEST_DIR=/tmp/dm_test
 CUR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 function run() {
-    script=$1
-    echo "Running test $script..."
-    TEST_DIR="$TEST_DIR" \
-    PATH="tests/_utils:$PATH" \
-    TEST_NAME="$(basename "$(dirname "$script")")" \
-    bash +x "$script"
-}
-
-function run2() {
     if [ $1 == "all" ];then 
         $CUR/run.sh
     else
@@ -25,7 +16,7 @@ function run2() {
 if [ "$#" -ge 1 ]; then
     test_case=$1
 else
-    run2 compatibility
+    run compatibility
     exit 0
 fi
 
@@ -33,10 +24,10 @@ echo "run test with current dm-master and previous dm-worker"
 cp $PWD/bin/dm-master.test.current $PWD/bin/dm-master.test
 cp $PWD/bin/dm-worker.test.previous $PWD/bin/dm-worker.test
 
-run2 "$test_case"
+run "$test_case"
 
 echo "run test with previous dm-master and current dm-worker"
 cp $PWD/bin/dm-master.test.previous $PWD/bin/dm-master.test
 cp $PWD/bin/dm-worker.test.current $PWD/bin/dm-worker.test
 
-run2 "$test_case"
+run "$test_case"
