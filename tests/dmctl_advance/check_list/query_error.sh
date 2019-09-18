108b6db2d01dacfd694a76178404e0a969a4eb18
Xuecheng Zhang <csuzhangxc@gmail.com>
2019-09-18 10:14:26 +0800

    *: refine dmctl output for `DM-master`, `task-name` and `config-file` (#291)
    (cherry picked from commit 4c39a6b1acadfacbf27b306e866aa52aab48b7da)

--- tests/dmctl_advance/check_list/query_error.sh
+++ tests/dmctl_advance/check_list/query_error.sh
@@ -3,7 +3,7 @@
 function query_error_wrong_arg() {
     run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
         "query-error wrong_args_count more_than_one" \
-        "query-error \[-w worker ...\] \[task_name\]" 1
+        "query-error \[-w worker ...\] \[task-name\]" 1
 }
 
 function query_error_while_master_down() {
--- tests/dmctl_basic/check_list/check_task.sh
+++ tests/dmctl_basic/check_list/check_task.sh
@@ -3,7 +3,7 @@
 function check_task_wrong_arg() {
     run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
         "check-task" \
-        "check-task <config_file> \[flags\]" 1
+        "check-task <config-file> \[flags\]" 1
 }
 
 function check_task_wrong_config_file() {
