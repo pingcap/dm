f80c5bcc6acf9f2d512b795aafbba8c0801f0e05
luancheng <luancheng@zhihu.com>
2019-09-25 14:33:57 +0800

    address comments

--- syncer/db.go
+++ syncer/db.go
@@ -179,6 +179,7 @@ func (conn *DBConn) querySQL(tctx *tcontext.Context, query string, args ...inter
 			return false
 		},
 	}
+
 	ret, _, err := conn.baseConn.ApplyRetryStrategy(
 		tctx,
 		params,
@@ -239,7 +240,6 @@ func (conn *DBConn) executeSQLWithIgnore(tctx *tcontext.Context, ignoreError fun
 		func(ctx *tcontext.Context) (interface{}, error) {
 			startTime := time.Now()
 			ret, err := conn.baseConn.ExecuteSQLWithIgnoreError(ctx, ignoreError, queries, args...)
-
 			if err == nil {
 				cost := time.Since(startTime)
 				txnHistogram.WithLabelValues(conn.cfg.Name).Observe(cost.Seconds())
