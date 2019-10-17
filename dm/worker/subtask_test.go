bdee293671c3369140930dba0a2721322a8cbc0a
amyangfei <amyangfei@gmail.com>
2019-10-17 15:29:29 +0800

    DM-worker: add error abstraction in pb.ProcessError (#297) (#320)

--- dm/worker/subtask_test.go
+++ dm/worker/subtask_test.go
@@ -99,7 +99,7 @@ func (m *MockUnit) Process(ctx context.Context, pr chan pb.ProcessResult) {
 			pr <- pb.ProcessResult{}
 		} else {
 			pr <- pb.ProcessResult{
-				Errors: append([]*pb.ProcessError{}, unit.NewProcessError(pb.ErrorType_UnknownError, errors.ErrorStack(err))),
+				Errors: append([]*pb.ProcessError{}, unit.NewProcessError(pb.ErrorType_UnknownError, err)),
 			}
 		}
 	}
