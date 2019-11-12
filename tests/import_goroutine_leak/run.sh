e0446c898af0373e2721ee61c8e2b16ea8813e30
GregoryIan <argregoryian@gmail.com>
2019-11-12 14:03:23 +0800

    fix goroutine leak in restoreData

--- tests/import_goroutine_leak/run.sh
+++ tests/import_goroutine_leak/run.sh
@@ -27,7 +27,8 @@ function run() {
     run_sql_file $WORK_DIR/db2.prepare.sql $MYSQL_HOST2 $MYSQL_PORT2
 
 
-    # check workers of import unit exit
+    echo "dm-worker paninc, doJob of import unit workers don't exit"
+    # check doJobs of import unit worker exit
     inject_points=("github.com/pingcap/dm/loader/dispatchError=return(1)"
                    "github.com/pingcap/dm/loader/LoadDataSlowDown=sleep(1000)"
                    "github.com/pingcap/dm/loader/executeSQLError=return(1)"
@@ -51,6 +52,26 @@ function run() {
     err_cnt=`grep "panic" $WORK_DIR/worker1/log/stdout.log | wc -l`
     if [ $err_cnt -ne 1 ]; then
         echo "dm-worker1 doesn't panic, panic count ${err_cnt}"
+        exit 2
+    fi
+
+    echo "dm-workers paninc again, workers of import unit don't exit"
+    # check workers of import unit exit
+    inject_points=("github.com/pingcap/dm/loader/dontWaitWorkerExit=return(1)"
+                   "github.com/pingcap/dm/loader/LoadDataSlowDown=sleep(1000)"
+                   "github.com/pingcap/dm/loader/executeSQLError=return(1)"
+                   )
+    export GO_FAILPOINTS="$(join_string \; ${inject_points[@]})"
+    run_dm_worker $WORK_DIR/worker1 $WORKER1_PORT $cur/conf/dm-worker1.toml
+    run_dm_worker $WORK_DIR/worker2 $WORKER2_PORT $cur/conf/dm-worker2.toml
+    sleep 2s
+    check_port_offline $WORKER1_PORT 20
+    check_port_offline $WORKER2_PORT 20
+
+    # dm-worker1 panics
+    err_cnt=`grep "panic" $WORK_DIR/worker1/log/stdout.log | wc -l`
+    if [ $err_cnt -ne 2 ]; then
+        echo "dm-worker1 doesn't panic again, panic count ${err_cnt}"
         exit 2
     fi
 
