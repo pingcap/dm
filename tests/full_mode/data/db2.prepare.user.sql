1c2e74e4310b6d1f8e6226d7693e52c65ee8c9c1
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2019-11-25 13:50:10 +0800

    refine full_mode test

--- tests/full_mode/data/db2.prepare.user.sql
+++ tests/full_mode/data/db2.prepare.user.sql
@@ -3,5 +3,5 @@ flush privileges;
 create user 'dm_full'@'%' identified by '';
 grant all privileges on *.* to 'dm_full'@'%';
 revoke replication slave, replication client on *.* from 'dm_full'@'%';
-revoke create temporary tables, lock tables, create routine, alter routine, event, create tablespace, file, shutdown, execute, process, index on *.* from 'dm_incremental'@'%'; # privileges not supported by TiDB
+revoke create temporary tables, lock tables, create routine, alter routine, event, create tablespace, file, shutdown, execute, process, index on *.* from 'dm_full'@'%'; # privileges not supported by TiDB
 flush privileges;
