84901c10dc3053b189452a05a62ba72d989323c6
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2019-11-25 10:38:35 +0800

    refine incremental mode integration tests

--- tests/incremental_mode/data/db1.prepare.user.sql
+++ tests/incremental_mode/data/db1.prepare.user.sql
@@ -1,5 +1,7 @@
 drop user if exists 'dm_incremental';
 flush privileges;
 create user 'dm_incremental'@'%' identified by '';
-grant replication slave, replication client on *.* to 'dm_incremental'@'%';
+grant all privileges on *.* to 'dm_incremental'@'%';
+revoke select, reload on *.* from 'dm_incremental'@'%';
+revoke create temporary tables, lock tables, create routine, alter routine, event, create tablespace, file, shutdown, execute, process, index on *.* from 'dm_incremental'@'%'; # privileges not supported by TiDB
 flush privileges;
