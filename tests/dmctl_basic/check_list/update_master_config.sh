d2fea94271b0a948d5e7b0585e73acb9d307c37b
csuzhangxc <csuzhangxc@gmail.com>
2019-12-03 13:14:16 +0800

    tests: fix CI

--- tests/dmctl_basic/check_list/update_master_config.sh
+++ tests/dmctl_basic/check_list/update_master_config.sh
@@ -23,7 +23,6 @@ function update_master_config_success() {
     master_conf=$1
     run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
         "update-master-config $master_conf" \
-        "\"result\": true" 3 \
-        "\"unit\": \"Sync\"" 2 \
-        "\"stage\": \"Running\"" 4
+        "\"result\": false" 1 \
+        "\"msg\": \"not supported now\"" 1
 }
