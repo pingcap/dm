78a93eb30d88d57aeb984105f9c573b2872af647
csuzhangxc <csuzhangxc@gmail.com>
2019-12-14 12:28:04 +0800

    tests: fix safe_mode, sequence_safe_mode

--- tests/sequence_safe_mode/run.sh
+++ tests/sequence_safe_mode/run.sh
@@ -37,6 +37,10 @@ function run() {
     run_dm_worker $WORK_DIR/worker2 $WORKER2_PORT $cur/conf/dm-worker2.toml
     check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER1_PORT
     check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER2_PORT
+
+    sleep 2
+    echo "stat task after reset failpoint"
+    dmctl_start_task
     check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
 
     pkill -hup dm-worker.test 2>/dev/null || true
@@ -44,15 +48,22 @@ function run() {
 
     export GO_FAILPOINTS='github.com/pingcap/dm/syncer/SequenceShardSyncedExecutionExit=return(true);github.com/pingcap/dm/syncer/SafeModeInitPhaseSeconds=return(300)'
 
+    echo "restart DM-worker after set SequenceShardSyncedExecutionExit failpoint"
     run_dm_worker $WORK_DIR/worker1 $WORKER1_PORT $cur/conf/dm-worker1.toml
     run_dm_worker $WORK_DIR/worker2 $WORKER2_PORT $cur/conf/dm-worker2.toml
     check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER1_PORT
     check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER2_PORT
 
+    sleep 2
+    echo "stat task after set SequenceShardSyncedExecutionExit failpoint"
+    dmctl_start_task
+    check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
+
     # DM-worker exit when waiting for sharding group synced
     run_sql_file $cur/data/db1.increment2.sql $MYSQL_HOST1 $MYSQL_PORT1
     run_sql_file $cur/data/db2.increment2.sql $MYSQL_HOST2 $MYSQL_PORT2
 
+    OWNER_PORT=""
     i=0
     while [ $i -lt 10 ]; do
         # we can't determine which DM-worker is the sharding lock owner, so we try both of them
@@ -64,6 +75,7 @@ function run() {
             run_dm_worker $WORK_DIR/worker1 $WORKER1_PORT $cur/conf/dm-worker1.toml
             check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER1_PORT
             check_instance_id="1"
+            OWNER_PORT=$WORKER1_PORT
             break
         fi
         # DM-worker2 is sharding lock owner and exits
@@ -74,6 +86,7 @@ function run() {
             run_dm_worker $WORK_DIR/worker2 $WORKER2_PORT $cur/conf/dm-worker2.toml
             check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER2_PORT
             check_instance_id="2"
+            OWNER_PORT=$WORKER2_PORT
             break
         fi
 
@@ -85,6 +98,13 @@ function run() {
         exit 1
     fi
 
+    sleep 2
+    echo "start task after restart DDL owner"
+    task_conf="$cur/conf/dm-task.yaml"
+    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+        "start-task $task_conf" \
+        "\"result\": true" 2 \
+        "\"worker\": \"127.0.0.1:$OWNER_PORT\"" 1
     check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
 
     $cur/../bin/check_safe_mode $check_instance_id
