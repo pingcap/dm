634d67bd6ff377f5edef1f63d481bb3594ef94af
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-01-03 19:38:10 +0800

    fix load_interrupt integration_tests

--- tests/load_interrupt/run.sh
+++ tests/load_interrupt/run.sh
@@ -42,12 +42,23 @@ function run() {
 
     export GO_FAILPOINTS="github.com/pingcap/dm/loader/LoadExceedOffsetExit=return($THRESHOLD)"
 
+    run_dm_master $WORK_DIR/master $MASTER_PORT $cur/conf/dm-master.toml
+    check_rpc_alive $cur/../bin/check_master_online 127.0.0.1:$MASTER_PORT
     run_dm_worker $WORK_DIR/worker1 $WORKER1_PORT $cur/conf/dm-worker1.toml
     check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER1_PORT
     run_dm_worker $WORK_DIR/worker2 $WORKER2_PORT $cur/conf/dm-worker2.toml
     check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER2_PORT
-    run_dm_master $WORK_DIR/master $MASTER_PORT $cur/conf/dm-master.toml
-    check_rpc_alive $cur/../bin/check_master_online 127.0.0.1:$MASTER_PORT
+    # operate mysql config to worker
+    cp $cur/conf/mysql1.toml $WORK_DIR/mysql1.toml
+    cp $cur/conf/mysql2.toml $WORK_DIR/mysql2.toml
+    sed -i "/relay-binlog-name/i\relay-dir = \"$WORK_DIR/worker1/relay_log\"" $WORK_DIR/mysql1.toml
+    sed -i "/relay-binlog-name/i\relay-dir = \"$WORK_DIR/worker2/relay_log\"" $WORK_DIR/mysql2.toml
+    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+        "operate-worker create $WORK_DIR/mysql1.toml" \
+        "true" 1
+    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+        "operate-worker create $WORK_DIR/mysql2.toml" \
+        "true" 1
 
     dmctl_start_task
 
