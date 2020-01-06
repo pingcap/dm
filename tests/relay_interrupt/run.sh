a59eb51b68e68ea1ff4199f9c6e8cdaa0dfc3127
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-01-06 20:11:53 +0800

    wait for time in relay_interrupt

--- tests/relay_interrupt/run.sh
+++ tests/relay_interrupt/run.sh
@@ -71,7 +71,7 @@ function run() {
         run_dm_worker $WORK_DIR/worker1 $WORKER1_PORT $cur/conf/dm-worker1.toml
         check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER1_PORT
 
-        sleep 2
+        sleep 5
         echo "start task after restarted dm-worker"
         task_conf="$cur/conf/dm-task.yaml"
         run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
