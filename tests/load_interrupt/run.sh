e12872f4662ede11eff1b9d88eeed8421e041094
csuzhangxc <csuzhangxc@gmail.com>
2020-02-08 11:47:13 +0800

    tests: try fix load_interrupt case

--- tests/load_interrupt/run.sh
+++ tests/load_interrupt/run.sh
@@ -82,13 +82,16 @@ function run() {
     check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
 
     # LoadExecCreateTableFailed error return twice
+    sleep 2
     err_cnt=`grep LoadExecCreateTableFailed $WORK_DIR/worker1/log/dm-worker.log | wc -l`
     if [ $err_cnt -ne 2 ]; then
         echo "error LoadExecCreateTableFailed's count is not 2"
         exit 2
     fi
 
-    sleep 1
+    # strange, TiDB (at least with mockTiKV) needs a long time to see the update of `test_loader_checkpoint`,
+    # and even later txn may see the older state than the earlier txn.
+    sleep 8
     run_sql "SELECT count(*) from dm_meta.test_loader_checkpoint where cp_schema = '$TEST_NAME' and offset = end_pos" $TIDB_PORT
     check_contains "count(*): 2"
 
