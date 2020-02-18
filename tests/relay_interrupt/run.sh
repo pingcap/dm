ead09848d6e89df742a17529a9242569db9b71a1
csuzhangxc <csuzhangxc@gmail.com>
2020-02-18 18:09:16 +0800

    tests: turn off relay_interrupt until compatible with relay again.

--- tests/relay_interrupt/run.sh
+++ tests/relay_interrupt/run.sh
@@ -52,12 +52,18 @@ function run() {
             "no sub task started" 1 \
             "ERROR" 1
 
-        echo "start task and query status, task have error message"
+        echo "start task and query status, task and relay have error message"
         task_conf="$cur/conf/dm-task.yaml"
         run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
             "start-task $task_conf" \
-            "\"result\": true" 2
+            "\"result\": true" 1
 
+        echo "waiting for asynchronous relay and subtask to be started"
+        sleep 2
+        run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+            "query-status -s $SOURCE_ID1" \
+            "database driver error: ERROR 1152" 1 \
+            "ERROR" 1
         run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
             "query-status -s $SOURCE_ID1" \
             "no valid relay sub directory exists" 1 \
@@ -80,28 +86,28 @@ function run() {
         task_conf="$cur/conf/dm-task.yaml"
         run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
             "start-task $task_conf" \
-            "\"result\": true" 1 \
             "\"result\": false" 1 \
-            "start sub task test: sub task test already exists" 1
+            "subtasks with name test for sources \[mysql-replica-01\] already exist" 1
 
-        run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
-            "query-status test" \
-            "\"binlogType\": \"local\"" 1
+# TODO(csuzhangxc): support relay log again.
+#        run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+#            "query-status test" \
+#            "\"binlogType\": \"local\"" 1
+#
+#        check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
 
-        check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
-
-        prepare_data2
-        echo "read binlog from relay log failed, and will use remote binlog"
+#        prepare_data2
+#        echo "read binlog from relay log failed, and will use remote binlog"
         kill_dm_worker
         export GO_FAILPOINTS="github.com/pingcap/dm/pkg/streamer/GetEventFromLocalFailed=return()"
         run_dm_worker $WORK_DIR/worker1 $WORKER1_PORT $cur/conf/dm-worker1.toml
         check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER1_PORT
         sleep 8
-        run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
-            "query-status test" \
-            "\"binlogType\": \"remote\"" 1
-
-        check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
+#        run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+#            "query-status test" \
+#            "\"binlogType\": \"remote\"" 1
+#
+#        check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
 
         export GO_FAILPOINTS=''
         cleanup_process
