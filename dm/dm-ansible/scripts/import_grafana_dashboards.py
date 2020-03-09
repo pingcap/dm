1b8deb538578abd35d799b37bed6a7fbb8dd1270
liubo <liubo@pingcap.com>
2020-03-09 16:27:04 +0800

    update dm-ansible, support ha (#497)
    * update dm-ansible, support ha
    
    * fix import grafana dashboards
    
    * support upgrade grafana

--- dm/dm-ansible/scripts/import_grafana_dashboards.py
+++ dm/dm-ansible/scripts/import_grafana_dashboards.py
@@ -35,9 +35,16 @@ def fill_dashboard_with_dest_config(dashboard, dest, type_='node'):
     dashboard['title'] = dest['titles'][type_]
     dashboard['id'] = None
     # pprint(dashboard)
-    for row in dashboard['rows']:
-        for panel in row['panels']:
-            panel['datasource'] = dest['datasource']
+    if 'rows' in dashboard:
+        panels = dashboard['rows']
+    else:
+        panels = dashboard['panels']
+    for row in panels:
+        if 'panels' in row:
+            for panel in row['panels']:
+                panel['datasource'] = dest['datasource']
+        else:
+            row['datasource'] = dest['datasource']
 
     if 'templating' in dashboard:
         for templating in dashboard['templating']['list']:
