ebcfa2f11f9af5ad552a53d6eeb75bf883c52d68
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-03-20 15:01:54 +0800

    address comments

--- dm/worker/join.go
+++ dm/worker/join.go
@@ -58,11 +58,11 @@ func (s *Server) JoinMaster(endpoints []string) error {
 		resp, err := client.RegisterWorker(ctx1, req)
 		cancel1()
 		if err != nil {
-			log.L().Error("fail to register worker: %s", zap.Error(err))
+			log.L().Error("fail to register worker", zap.Error(err))
 			continue
 		}
 		if !resp.GetResult() {
-			log.L().Error("fail to register worker: %s", zap.String("error", resp.Msg))
+			log.L().Error("fail to register worker", zap.String("error", resp.Msg))
 			continue
 		}
 		return nil
