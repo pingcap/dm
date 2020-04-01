77990c59e4c77ef1ff65d3cd3f9d3f247380d0e4
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-04-01 23:55:02 +0800

    refine integration_test

--- tests/sharding2/data/db2.increment.sql
+++ tests/sharding2/data/db2.increment.sql
@@ -1,15 +1,2 @@
 create database sharding22;
 create table sharding22.t1 (id int primary key);
-alter table sharding22.t1 add column name varchar(20);
-alter table sharding22.t1 add index abc(name);
- 
-insert into sharding22.t1 values(10, "d"), (11, "e");
-
-alter table sharding22.t1 add column c int;
-alter table sharding22.t1 add index c(c);
-alter table sharding22.t1 add column d int;
-alter table sharding22.t1 add index d(d);
-alter table sharding22.t1 add column e int, add index e(e);
-
-insert into sharding22.t1 values(12, "g", 12, 12, 12);
-insert into sharding22.t1 values(13, "h", 13, 13, 13);
