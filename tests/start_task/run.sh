2c3def5a54148b2ef015067f3d840f7fff94aff6
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-04-02 23:53:54 +0800

    fix start_task test

--- tests/start_task/run.sh
+++ tests/start_task/run.sh
@@ -10,9 +10,9 @@ WORK_DIR=$TEST_DIR/$TEST_NAME
 function prepare_data() {
     run_sql 'DROP DATABASE if exists start_task;' $MYSQL_PORT1 $MYSQL_PASSWORD1
     run_sql 'CREATE DATABASE start_task;' $MYSQL_PORT1 $MYSQL_PASSWORD1
-    run_sql "CREATE TABLE start_task.t(i TINYINT, j INT UNIQUE KEY);" $MYSQL_PORT1 $MYSQL_PASSWORD1
+    run_sql "CREATE TABLE start_task.t${j}(i TINYINT, j INT UNIQUE KEY);" $MYSQL_PORT1 $MYSQL_PASSWORD1
     for j in $(seq 100); do
-        run_sql "INSERT INTO start_task.t VALUES ($j,${j}000$j),($j,${j}001$j);" $MYSQL_PORT1 $MYSQL_PASSWORD1
+        run_sql "INSERT INTO start_task.t${j} VALUES ($j,${j}000$j),($j,${j}001$j);" $MYSQL_PORT1 $MYSQL_PASSWORD1
     done
 }
 
@@ -29,6 +29,7 @@ function run() {
         echo "failpoint=${failpoints[i]}"
         export GO_FAILPOINTS=${failpoints[i]}
 
+        run_sql 'DROP DATABASE if exists start_task;' $TIDB_PORT $TIDB_PASSWORD
         prepare_data
 
         run_dm_master $WORK_DIR/master $MASTER_PORT $cur/conf/dm-master.toml
