39ec9b2ae7cfda8374a66ad524da80f6214810e7
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-04-21 15:58:45 +0800

    add integration_test

---
+++ tests/sequence_sharding/data/db1.increment2.sql
@@ -0,0 +1,4 @@
+use sharding_seq;
+insert into t1 values(15, "i", 15, 15, 15, 15);
+alter table t1 drop column f;
+alter table t2 drop column f;
--- tests/sequence_sharding/data/db2.increment.sql
+++ tests/sequence_sharding/data/db2.increment.sql
@@ -25,3 +25,7 @@ update t4 set d = 200;
 update t4 set uid=uid+100000;
 insert into t2 (uid,name,c) values(300003,'nvWgBf',73),(300004,'nD1000',4029);
 insert into t3 (uid,name,c) values(400004,'1000',1000);
+
+alter table t2 add column f int;
+alter table t3 add column f int;
+alter table t4 add column f int;
