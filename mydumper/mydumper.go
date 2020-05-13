6397ea1b64eb5d4a6170b4a35b134e69b2a2729b
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2020-05-13 18:16:25 +0800

    refine mydumper exits error log

--- mydumper/mydumper.go
+++ mydumper/mydumper.go
@@ -111,7 +111,11 @@ func (m *Mydumper) Process(ctx context.Context, pr chan pb.ProcessResult) {
 		}
 	}
 
-	m.logger.Info("dump data finished", zap.Duration("cost time", time.Since(begin)))
+	if len(errs) == 0 {
+		m.logger.Info("dump data finished", zap.Duration("cost time", time.Since(begin)))
+	} else {
+		m.logger.Error("dump data exits with error", zap.Duration("cost time", time.Since(begin)), log.ShortError(err))
+	}
 
 	pr <- pb.ProcessResult{
 		IsCanceled: isCanceled,
