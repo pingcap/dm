a39f9a8024d439176c07d11b1a5c5f5e0d4b6c62
gmhdbjd <gmhdbjd@gmail.com>
2020-06-02 23:23:43 +0800

    add timeout value

--- dm/master/election_test.go
+++ dm/master/election_test.go
@@ -33,7 +33,7 @@ type testElectionSuite struct {
 }
 
 func (t *testMaster) TestFailToStartLeader(c *check.C) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
 	// create a new cluster
