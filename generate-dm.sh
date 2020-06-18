33c7ede279c89c80daeb3497ea326058462038ef
Xuecheng Zhang <csuzhangxc@gmail.com>
2020-06-18 15:57:41 +0800

    track tool dependencies in the repo (#752)

--- generate-dm.sh
+++ generate-dm.sh
@@ -1,38 +1,66 @@
 #!/usr/bin/env bash
 
-cd dm/proto
+# If the environment variable is unset, GOPATH defaults
+# to a subdirectory named "go" in the user's home directory
+# ($HOME/go on Unix, %USERPROFILE%\go on Windows),
+# unless that directory holds a Go distribution.
+# Run "go env GOPATH" to see the current GOPATH.
+GOPATH=$(go env GOPATH)
+echo "using GOPATH=$GOPATH"
+
+# we use `retool` to manage the tools written in Go.
+# ensure it matches `-tool-dir` of `retool`.
+TOOLS_BIN_DIR=$(pwd)/_tools/bin
+
+# use `protoc-gen-gogofaster` rather than `protoc-gen-go`.
+GOGO_FASTER=$TOOLS_BIN_DIR/protoc-gen-gogofaster
+if [ ! -f ${GOGO_FASTER} ]; then
+    echo "${GOGO_FASTER} does not exist, please run 'make retool_setup' first"
+    exit 1
+fi
+
+# get and construct the path to gogo/protobuf.
+GO111MODULE=on go get -d github.com/gogo/protobuf
+GOGO_MOD=$(GO111MODULE=on go list -m github.com/gogo/protobuf)
+GOGO_PATH=$GOPATH/pkg/mod/${GOGO_MOD// /@}
+if [ ! -d ${GOGO_PATH} ]; then
+    echo "${GOGO_PATH} does not exist, please ensure 'github.com/gogo/protobuf' is in go.mod"
+    exit 1
+fi
+
+# we need `grpc-gateway` to generate HTTP API from gRPC API.
+GRPC_GATEWAY=$TOOLS_BIN_DIR/protoc-gen-grpc-gateway
+if [ ! -f ${GRPC_GATEWAY} ]; then
+    echo "${GRPC_GATEWAY} does not exist, please run 'make retool_setup' first"
+    exit 1
+fi
+
+# fetch `github.com/googleapis/googleapis`, seems no `.go` file in it,
+# so neither `go.mod` nor `retool` can track it.
+# hardcode the version here now.
+GO111MODULE=on go get github.com/googleapis/googleapis@91ef2d9
+GAPI_MOD=$(GO111MODULE=on go list -m github.com/googleapis/googleapis)
+GAPI_PATH=$GOPATH/pkg/mod/${GAPI_MOD// /@}
+GO111MODULE=on go mod tidy  # keep `go.mod` and `go.sum` tidy
+if [ ! -d ${GAPI_PATH} ]; then
+    echo "${GAPI_PATH} does not exist, try verify it manually"
+    exit 1
+fi
+
+cd dm/proto || exit 1
 
 echo "generate dm protobuf code..."
-GOGO_ROOT=${GOPATH}/src/github.com/gogo/protobuf
-if [ ! -d $GOGO_ROOT ]; then
-		echo "please use the following command to get gogo."
-		echo "go get -u github.com/gogo/protobuf/protoc-gen-gogofaster"
-		exit 1
-fi
 
-which protoc-gen-grpc-gateway
-if [ $? -ne 0 ]; then
-	echo "please use the following command to get grpc-gateway."
-	echo "go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
-	exit 1
-fi
+cp -r ${GAPI_PATH}/google ./
 
-GOOGLE_API_ROOT=${GOPATH}/src/github.com/googleapis/googleapis
-if [ ! -d $GOOGLE_API_ROOT ]; then
-	echo "please use the following command to get google apis."
-	echo "go get -u github.com/googleapis/googleapis/google/api"
-	exit 1
-fi
+protoc -I.:${GOGO_PATH}:${GOGO_PATH}/protobuf --plugin=${GOGO_FASTER} --gogofaster_out=plugins=grpc:../pb/ *.proto
 
-cp -r ${GOPATH}/src/github.com/googleapis/googleapis/google ./
+protoc -I.:${GOGO_PATH}:${GOGO_PATH}/protobuf --plugin=${GRPC_GATEWAY} --grpc-gateway_out=logtostderr=true:../pb/ dmmaster.proto
 
-protoc -I.:${GOGO_ROOT}:${GOGO_ROOT}/protobuf --gogofaster_out=plugins=grpc:../pb/ *.proto
-
-protoc -I.:${GOGO_ROOT}:${GOGO_ROOT}/protobuf --grpc-gateway_out=logtostderr=true:../pb/ dmmaster.proto
-
+chmod -R +w ./google  # permission is `-r--r--r--`
 rm -r ./google
 
-cd ../pb
+cd ../pb || exit 1
 sed -i.bak -E 's/import _ \"gogoproto\"//g' *.pb.go
 sed -i.bak -E 's/import fmt \"fmt\"//g' *.pb.go
 rm -f *.bak
