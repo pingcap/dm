010c3c98db4dd7567e32a16354d0ec18b53b087a
GMHDBJD <35025882+GMHDBJD@users.noreply.github.com>
2020-06-24 01:39:18 -0700

    error_message: add workaround for error (#753)

--- tests/relay_interrupt/run.sh
+++ tests/relay_interrupt/run.sh
@@ -51,7 +51,7 @@ function run() {
         run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
             "query-status -s $SOURCE_ID1" \
             "no sub task started" 1 \
-            "ERROR" 1
+            "ERROR" 2
 
         echo "start task and query status, task and relay have error message"
         task_conf="$cur/conf/dm-task.yaml"
@@ -65,11 +65,11 @@ function run() {
         run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
             "query-status -s $SOURCE_ID1" \
             "database driver error: ERROR 1152" 1 \
-            "ERROR" 1
+            "ERROR" 2
         run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
             "query-status -s $SOURCE_ID1" \
             "there aren't any data under relay log directory" 1 \
-            "ERROR" 1
+            "ERROR" 2
         run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
             "query-status" \
             "\"taskName\": \"test\"" 1 \
