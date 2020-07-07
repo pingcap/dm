6c37e3da11cbd62fc48c66e55ec69f8f85dd463e
gmhdbjd <gmhdbjd@gmail.com>
2020-07-07 19:50:31 +0800

    address comment

--- tests/dmctl_basic/check_list/check_task.sh
+++ tests/dmctl_basic/check_list/check_task.sh
@@ -40,5 +40,5 @@ function check_task_error_database_config() {
     run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
         "check-task $task_conf" \
         "Access denied for user" 1 \
-        "Please check the database config in configuration file"
+        "Please check the database config in configuration file" 1
 }
