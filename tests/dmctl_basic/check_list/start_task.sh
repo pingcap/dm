ed9c483e25b12fa29cbb2982828092c51d3eae3b
GMHDBJD <35025882+GMHDBJD@users.noreply.github.com>
2020-07-08 15:49:29 +0800

    dmctl: check master connection when start. (#786)

--- tests/dmctl_basic/check_list/start_task.sh
+++ tests/dmctl_basic/check_list/start_task.sh
@@ -11,10 +11,3 @@ function start_task_wrong_config_file() {
         "start-task not_exists_config_file" \
         "get file content error" 1
 }
-
-function start_task_while_master_down() {
-    task_conf=$1
-    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
-        "start-task $task_conf" \
-        "can not start task:" 1
-}
