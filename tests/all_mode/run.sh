628e7192d63feeee9f21d56234d755438c56d5b0
xiang <xiang13225080@163.com>
2020-07-16 12:48:01 +0800

    fix test

--- tests/all_mode/run.sh
+++ tests/all_mode/run.sh
@@ -76,7 +76,6 @@ function run() {
     dmctl_operate_source create $WORK_DIR/source1.toml $SOURCE_ID1
     dmctl_operate_source create $WORK_DIR/source2.toml $SOURCE_ID2
 
-
     # start DM task only
     dmctl_start_task "$cur/conf/dm-task.yaml" "--remove-meta"
 
@@ -108,11 +107,11 @@ function run() {
     sleep 2
 
     # wait for task running
-    check_http_alive 127.0.0.1:$MASTER_PORT/apis/${API_VERSION}/status/test '"name":"test","stage":"Running"' 10
+    check_http_alive 127.0.0.1:$MASTER_PORT/apis/${API_VERSION}/status/test '"stage": "Running"' 10
     sleep 2 # still wait for subtask running on other dm-workers
 
     # wait for task running
-    check_http_alive 127.0.0.1:$MASTER_PORT/apis/${API_VERSION}/status/test '"name":"test","stage":"Running"' 10
+    check_http_alive 127.0.0.1:$MASTER_PORT/apis/${API_VERSION}/status/test '"stage": "Running"' 10
     sleep 2 # still wait for subtask running on other dm-workers
 
     # kill tidb
