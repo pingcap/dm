8d5537980a781a13edf92d8c56709dfa8fa1bcec
lance6716 <lance6716@gmail.com>
2020-07-23 19:54:16 +0800

    Revert "worker, utils: show same master status"
    This reverts commit e3226d6bbbcbca575286f906e5289b1fc89ae58c.

--- dm/worker/server.go
+++ dm/worker/server.go
@@ -15,7 +15,6 @@ package worker
 
 import (
 	"context"
-	"github.com/pingcap/dm/pkg/utils"
 	"io"
 	"net"
 	"sync"
@@ -251,14 +250,12 @@ func (s *Server) QueryTaskOperation(ctx context.Context, req *pb.QueryTaskOperat
 func (s *Server) QueryStatus(ctx context.Context, req *pb.QueryStatusRequest) (*pb.QueryStatusResponse, error) {
 	log.L().Info("", zap.String("request", "QueryStatus"), zap.Stringer("payload", req))
 
-	utils.EnableMasterStatusCache()
 	resp := &pb.QueryStatusResponse{
 		Result:        true,
 		SubTaskStatus: s.worker.QueryStatus(req.Name),
 		RelayStatus:   s.worker.relayHolder.Status(),
 		SourceID:      s.worker.cfg.SourceID,
 	}
-	utils.DisableMasterStatusCache()
 
 	if len(resp.SubTaskStatus) == 0 {
 		resp.Msg = "no sub task started"
