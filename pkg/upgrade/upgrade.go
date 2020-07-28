1bee24138a9a133022354f7e59417631c971318a
csuzhangxc <csuzhangxc@gmail.com>
2020-07-28 18:38:42 +0800

    feat: add simple upgrade flow

---
+++ pkg/upgrade/upgrade.go
@@ -0,0 +1,76 @@
+// Copyright 2020 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package upgrade
+
+import "go.etcd.io/etcd/clientv3"
+
+var (
+	// upgrades records all functions used to upgrade from one version to the later version.
+	upgrades = []func(cli *clientv3.Client) error{
+		upgradeToVer1,
+	}
+)
+
+// ImportFromV10x does special upgrade/import handle from DM v1.0.x to v2.0.x.
+func ImportFromV10x(cli *clientv3.Client) error {
+	// TODO(csuzhangxc)
+	return nil
+}
+
+// TryUpgrade tries to upgrade the cluster from an older version to a new version.
+// This methods should often be called only one time after a DM-master started and it's the leader of the cluster.
+func TryUpgrade(cli *clientv3.Client) error {
+	// 1. get previous version from etcd.
+	preVer, _, err := GetVersion(cli)
+	if err != nil {
+		return err
+	}
+
+	// 2. check if any previous version exists.
+	if preVer.NotSet() {
+		// no initialization operations exist for Ver1 now,
+		// and any operations (may includes `upgrades`) if needed later.
+		// put the current version into etcd.
+		_, err = PutVersion(cli, CurrentVersion)
+		return err
+	}
+
+	// 3. compare the previous version with the current version.
+	if cmp := preVer.Compare(CurrentVersion); cmp == 0 {
+		// previous == current version, no need to upgrade.
+		return nil
+	} else if cmp > 0 {
+		// previous >= current version, this often means a older version of DM-master become the leader after started,
+		// do nothing for this now.
+		return nil
+	}
+
+	// 4. do upgrade operations.
+	for _, upgrade := range upgrades {
+		err = upgrade(cli)
+		if err != nil {
+			return err
+		}
+	}
+
+	// 5. put the current version into etcd.
+	_, err = PutVersion(cli, CurrentVersion)
+	return err
+}
+
+// upgradeToVer1 does upgrade operations from Ver0 to Ver1.
+// in fact, this do nothing now, and just for demonstration.
+func upgradeToVer1(cli *clientv3.Client) error {
+	return nil
+}
--- pkg/upgrade/upgrade_test.go
+++ pkg/upgrade/upgrade_test.go
@@ -46,3 +46,67 @@ func clearTestData(c *C) {
 type testForEtcd struct{}
 
 var _ = Suite(&testForEtcd{})
+
+func (t *testForEtcd) TestTryUpgrade(c *C) {
+	defer clearTestData(c)
+
+	// mock upgrade functions.
+	oldUpgrades := upgrades
+	defer func() {
+		upgrades = oldUpgrades
+	}()
+	mockVerNo := uint64(0)
+	upgrades = []func(cli *clientv3.Client) error{
+		func(cli *clientv3.Client) error {
+			mockVerNo = currentInternalNo + 1
+			return nil
+		},
+	}
+
+	// no previous version exist, new cluster.
+	ver, rev1, err := GetVersion(etcdTestCli)
+	c.Assert(err, IsNil)
+	c.Assert(rev1, Greater, int64(0))
+	c.Assert(ver.NotSet(), IsTrue)
+
+	// try to upgrade, but do nothing except the current version recorded.
+	c.Assert(TryUpgrade(etcdTestCli), IsNil)
+	ver, rev2, err := GetVersion(etcdTestCli)
+	c.Assert(err, IsNil)
+	c.Assert(rev2, Greater, rev1)
+	c.Assert(ver, DeepEquals, CurrentVersion)
+	c.Assert(mockVerNo, Equals, uint64(0))
+
+	// try to upgrade again, do nothing because the version is the same.
+	c.Assert(TryUpgrade(etcdTestCli), IsNil)
+	ver, rev3, err := GetVersion(etcdTestCli)
+	c.Assert(err, IsNil)
+	c.Assert(rev3, Equals, rev2)
+	c.Assert(ver, DeepEquals, CurrentVersion)
+	c.Assert(mockVerNo, Equals, uint64(0))
+
+	// mock a newer current version.
+	oldCurrentVer := CurrentVersion
+	defer func() {
+		CurrentVersion = oldCurrentVer
+	}()
+	newerVer := NewVersion(currentInternalNo+1, "mock-current-ver")
+	CurrentVersion = newerVer
+
+	// try to upgrade, to a newer version, upgrade operations applied.
+	c.Assert(TryUpgrade(etcdTestCli), IsNil)
+	ver, rev4, err := GetVersion(etcdTestCli)
+	c.Assert(err, IsNil)
+	c.Assert(rev4, Greater, rev3)
+	c.Assert(ver, DeepEquals, newerVer)
+	c.Assert(mockVerNo, Equals, currentInternalNo+1)
+
+	// try to upgrade, to an older version, do nothing.
+	CurrentVersion = oldCurrentVer
+	c.Assert(TryUpgrade(etcdTestCli), IsNil)
+	ver, rev5, err := GetVersion(etcdTestCli)
+	c.Assert(err, IsNil)
+	c.Assert(rev5, Equals, rev4)
+	c.Assert(ver, DeepEquals, newerVer) // not changed.
+	c.Assert(mockVerNo, Equals, currentInternalNo+1)
+}
