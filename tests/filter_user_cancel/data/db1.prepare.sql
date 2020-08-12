2ff4d84f351bdf90f26904e908722fa92b15e44b
gmhdbjd <gmhdbjd@gmail.com>
2020-08-12 16:46:48 +0800

    update test

--- tests/filter_user_cancel/data/db1.prepare.sql
+++ tests/filter_user_cancel/data/db1.prepare.sql
@@ -1,5 +1,5 @@
 drop database if exists `filter_user_cancel`;
 create database `filter_user_cancel`;
 use `filter_user_cancel`;
-create table t1 (id int, name varchar(20));
+create table t1 (id int, name varchar(20), primary key(`id`));
 insert into t1 (id, name) values (1, 'arya'), (2, 'catelyn');
