2170628321ad2ffa9dd4494c692736c42da4871a
lance6716 <lance6716@gmail.com>
2020-08-12 13:27:18 +0800

    fix test

--- syncer/dml_test.go
+++ syncer/dml_test.go
@@ -27,16 +27,16 @@ func (s *testSyncerSuite) TestCastUnsigned(c *C) {
 		Type     string
 		expected interface{}
 	}{
-		{int8(-math.Exp2(7)), false, mysql.TypeTiny, int8(-math.Exp2(7))}, // TINYINT
-		{int8(-math.Exp2(7)), true, mysql.TypeTiny, uint8(math.Exp2(7))},
-		{int16(-math.Exp2(15)), false, mysql.TypeShort, int16(-math.Exp2(15))}, //SMALLINT
-		{int16(-math.Exp2(15)), true, mysql.TypeShort, uint16(math.Exp2(15))},
-		{int32(-math.Exp2(23)), false, mysql.TypeInt24, int32(-math.Exp2(23))}, //MEDIUMINT
-		{int32(-math.Exp2(23)), true, mysql.TypeInt24, uint32(math.Exp2(23))},
-		{int32(-math.Exp2(31)), false, mysql.TypeLong, int32(-math.Exp2(31))}, // INT
-		{int32(-math.Exp2(31)), true, mysql.TypeLong, uint32(math.Exp2(31))},
-		{int64(-math.Exp2(63)), false, mysql.TypeLonglong, int64(-math.Exp2(63))}, // BIGINT
-		{int64(-math.Exp2(63)), true, mysql.TypeLonglong, uint64(math.Exp2(63))},
+		{int8(-math.Exp2(7)), false, "tinyint(4)", int8(-math.Exp2(7))}, // TINYINT
+		{int8(-math.Exp2(7)), true, "tinyint(3) unsigned", uint8(math.Exp2(7))},
+		{int16(-math.Exp2(15)), false, "smallint(6)", int16(-math.Exp2(15))}, //SMALLINT
+		{int16(-math.Exp2(15)), true, "smallint(5) unsigned", uint16(math.Exp2(15))},
+		{int32(-math.Exp2(23)), false, "mediumint(9)", int32(-math.Exp2(23))}, //MEDIUMINT
+		{int32(-math.Exp2(23)), true, "mediumint(8) unsigned", uint32(math.Exp2(23))},
+		{int32(-math.Exp2(31)), false, "int(11)", int32(-math.Exp2(31))}, // INT
+		{int32(-math.Exp2(31)), true, "int(10) unsigned", uint32(math.Exp2(31))},
+		{int64(-math.Exp2(63)), false, "bigint(20)", int64(-math.Exp2(63))}, // BIGINT
+		{int64(-math.Exp2(63)), true, "bigint(20) unsigned", uint64(math.Exp2(63))},
 	}
 	for _, cs := range cases {
 		obtained := castUnsigned(cs.data, cs.unsigned, cs.Type)
