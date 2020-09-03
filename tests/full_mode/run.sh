a7c558392e4a09c1d90e1896d5baa1c9753d58c9
lance6716 <lance6716@gmail.com>
2020-09-03 10:38:26 +0800

    test: add test to verify `/` in db and table names

--- tests/full_mode/run.sh
+++ tests/full_mode/run.sh
@@ -59,6 +59,51 @@ function fail_acquire_global_lock() {
     cleanup_process $*
 }
 
+function escape_schema() {
+    cp $cur/data/db1.prepare.sql $WORK_DIR/db1.prepare.sql
+    cp $cur/data/db2.prepare.sql $WORK_DIR/db2.prepare.sql
+    cp $cur/conf/dm-task.yaml $WORK_DIR/dm-task.yaml
+    cp $cur/conf/diff_config.toml $WORK_DIR/diff_config.toml
+    sed -i "s/full_mode/full\/mode/g" $WORK_DIR/db1.prepare.sql $WORK_DIR/db2.prepare.sql $WORK_DIR/dm-task.yaml $WORK_DIR/diff_config.toml
+    sed -i "/timezone/i\clean-dump-file: false" $WORK_DIR/dm-task.yaml
+
+    run_sql_file $cur/data/db1.prepare.sql $MYSQL_HOST1 $MYSQL_PORT1 $MYSQL_PASSWORD1
+    check_contains 'Query OK, 2 rows affected'
+    run_sql_file $cur/data/db2.prepare.sql $MYSQL_HOST2 $MYSQL_PORT2 $MYSQL_PASSWORD2
+    check_contains 'Query OK, 3 rows affected'
+
+    # test load data with `/` in the table name
+    run_sql_source1 "create table full_mode.\`tb\/1\` (id int,name varchar(10), primary key(\`id\`));"
+    run_sql_source1 "insert into full_mode.\`tb\/1\` values(1,'haha');"
+    run_sql_source1 "insert into full_mode.\`tb\/1\` values(2,'hihi');"
+
+    run_sql_file $cur/data/db1.prepare.user.sql $MYSQL_HOST1 $MYSQL_PORT1 $MYSQL_PASSWORD1
+    check_count 'Query OK, 0 rows affected' 7
+    run_sql_file $cur/data/db2.prepare.user.sql $MYSQL_HOST2 $MYSQL_PORT2 $MYSQL_PASSWORD2
+    check_count 'Query OK, 0 rows affected' 7
+
+    run_dm_master $WORK_DIR/master $MASTER_PORT $cur/conf/dm-master.toml
+    check_rpc_alive $cur/../bin/check_master_online 127.0.0.1:$MASTER_PORT
+    run_dm_worker $WORK_DIR/worker1 $WORKER1_PORT $cur/conf/dm-worker1.toml
+    check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER1_PORT
+    run_dm_worker $WORK_DIR/worker2 $WORKER2_PORT $cur/conf/dm-worker2.toml
+    check_rpc_alive $cur/../bin/check_worker_online 127.0.0.1:$WORKER2_PORT
+    # operate mysql config to worker
+    cp $cur/conf/source1.yaml $WORK_DIR/source1.yaml
+    cp $cur/conf/source2.yaml $WORK_DIR/source2.yaml
+    sed -i "/relay-binlog-name/i\relay-dir: $WORK_DIR/worker1/relay_log" $WORK_DIR/source1.yaml
+    sed -i "/relay-binlog-name/i\relay-dir: $WORK_DIR/worker2/relay_log" $WORK_DIR/source2.yaml
+    dmctl_operate_source create $WORK_DIR/source1.yaml $SOURCE_ID1
+    dmctl_operate_source create $WORK_DIR/source2.yaml $SOURCE_ID2
+
+    # start DM task only
+    dmctl_start_task "$cur/conf/dm-task.yaml" "--remove-meta"
+    check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
+
+    cleanup_data full/mode
+    cleanup_process $*
+}
+
 function run() {
     fail_acquire_global_lock
 
