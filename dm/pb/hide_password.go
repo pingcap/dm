c8d4e9d8e920d0e0f8893cafe330dafa7c166eb3
lance6716 <lance6716@gmail.com>
2020-09-08 17:34:06 +0800

    fix hound

--- dm/pb/hide_password.go
+++ dm/pb/hide_password.go
@@ -5,17 +5,19 @@ import (
 )
 
 var (
-	// avoid cycle import, this function should be overwrite by utils.HidePassword
+	// HidePwdFunc should be overwrite by utils.HidePassword, this variable is for avoiding cycle import
 	HidePwdFunc = func(s string) string {
 		return s
 	}
 )
 
+// MarshalLogObject implements zapcore.ObjectMarshaler
 func (m *StartTaskRequest) MarshalLogObject(enc zapcore.ObjectEncoder) error {
 	enc.AddString("HidePasswordObject", HidePwdFunc(m.String()))
 	return nil
 }
 
+// MarshalLogObject implements zapcore.ObjectMarshaler
 func (m *OperateSourceRequest) MarshalLogObject(enc zapcore.ObjectEncoder) error {
 	enc.AddString("HidePasswordObject", HidePwdFunc(m.String()))
 	return nil
