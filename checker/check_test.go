14c4e2fc5104302cc7b9846bdbd1f2e592299b54
gmhdbjd <gmhdbjd@gmail.com>
2020-09-09 18:27:30 +0800

    address comment

--- checker/check_test.go
+++ checker/check_test.go
@@ -63,7 +63,7 @@ func ignoreExcept(itemMap map[string]struct{}) []string {
 		config.ShardTableSchemaChecking,
 		config.ShardAutoIncrementIDChecking,
 	}
-	ignoreCheckingItems := make([]string, 0)
+	ignoreCheckingItems := make([]string, len(items)-len(itemMap))
 	for _, i := range items {
 		if _, ok := itemMap[i]; !ok {
 			ignoreCheckingItems = append(ignoreCheckingItems, i)
