8727b640d7c581df12cd755447a77d1a835ede8b
csuzhangxc <csuzhangxc@gmail.com>
2020-09-11 12:49:23 +0800

    master: fix data race

--- dm/master/server_test.go
+++ dm/master/server_test.go
@@ -1333,10 +1333,13 @@ func (t *testMaster) TestOfflineMember(c *check.C) {
 	var leaderID string
 	// ensure s2 has got the right leader info, because it will be used to `OfflineMember`.
 	c.Assert(utils.WaitSomething(30, 100*time.Millisecond, func() bool {
-		if s2.leader == "" {
+		s2.RLock()
+		leader := s2.leader
+		s2.RUnlock()
+		if leader == "" {
 			return false
 		}
-		if s2.leader == oneselfLeader {
+		if leader == oneselfLeader {
 			leaderID = s2.cfg.Name
 		} else {
 			leaderID = s2.leader
