dc33d8f98f261bc21c9aab1502ecc9a894b94205
csuzhangxc <csuzhangxc@gmail.com>
2020-09-17 18:57:24 +0800

    utils: add test cases for `FetchAllDoTables` and refine code

--- pkg/utils/common_test.go
+++ pkg/utils/common_test.go
@@ -15,9 +15,12 @@ package utils
 
 import (
 	"bytes"
+	"fmt"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	. "github.com/pingcap/check"
 	"github.com/pingcap/parser"
+	"github.com/pingcap/tidb-tools/pkg/filter"
 )
 
 var _ = Suite(&testCommonSuite{})
@@ -50,5 +53,92 @@ func (s *testCommonSuite) TestTrimCtrlChars(c *C) {
 		_, err := parser2.ParseOneStmt(newDDL, "", "")
 		c.Assert(err, IsNil)
 		buf.Reset()
+	}
+}
+
+func (s *testCommonSuite) TestFetchAllDoTables(c *C) {
+	db, mock, err := sqlmock.New()
+	c.Assert(err, IsNil)
+
+	// empty filter, exclude system schemas
+	ba, err := filter.New(false, nil)
+	c.Assert(err, IsNil)
+
+	// no schemas need to do.
+	mock.ExpectQuery(`SHOW DATABASES`).WillReturnRows(sqlmock.NewRows([]string{"Database"}))
+	got, err := FetchAllDoTables(db, ba)
+	c.Assert(err, IsNil)
+	c.Assert(got, HasLen, 0)
+	mock.ExpectationsWereMet()
+
+	// only system schemas exist, still no need to do.
+	schemas := []string{"information_schema", "mysql", "performance_schema", "sys", filter.DMHeartbeatSchema}
+	rows := sqlmock.NewRows([]string{"Database"})
+	s.addRowsForSchemas(rows, schemas)
+	mock.ExpectQuery(`SHOW DATABASES`).WillReturnRows(rows)
+	got, err = FetchAllDoTables(db, ba)
+	c.Assert(err, IsNil)
+	c.Assert(got, HasLen, 0)
+	mock.ExpectationsWereMet()
+
+	// schemas without tables in them.
+	doSchema := "test_db"
+	schemas = []string{"information_schema", "mysql", "performance_schema", "sys", filter.DMHeartbeatSchema, doSchema}
+	rows = sqlmock.NewRows([]string{"Database"})
+	s.addRowsForSchemas(rows, schemas)
+	mock.ExpectQuery(`SHOW DATABASES`).WillReturnRows(rows)
+	mock.ExpectQuery(fmt.Sprintf("SHOW FULL TABLES IN `%s` WHERE Table_Type != 'VIEW'", doSchema)).WillReturnRows(
+		sqlmock.NewRows([]string{fmt.Sprintf("Tables_in_%s", doSchema), "Table_type"}))
+	got, err = FetchAllDoTables(db, ba)
+	c.Assert(err, IsNil)
+	c.Assert(got, HasLen, 0)
+	mock.ExpectationsWereMet()
+
+	// do all tables under the schema.
+	rows = sqlmock.NewRows([]string{"Database"})
+	s.addRowsForSchemas(rows, schemas)
+	mock.ExpectQuery(`SHOW DATABASES`).WillReturnRows(rows)
+	tables := []string{"tbl1", "tbl2", "exclude_tbl"}
+	rows = sqlmock.NewRows([]string{fmt.Sprintf("Tables_in_%s", doSchema), "Table_type"})
+	s.addRowsForTables(rows, tables)
+	mock.ExpectQuery(fmt.Sprintf("SHOW FULL TABLES IN `%s` WHERE Table_Type != 'VIEW'", doSchema)).WillReturnRows(rows)
+	got, err = FetchAllDoTables(db, ba)
+	c.Assert(err, IsNil)
+	c.Assert(got, HasLen, 1)
+	c.Assert(got[doSchema], DeepEquals, tables)
+	mock.ExpectationsWereMet()
+
+	// use a block-allow-list to fiter some tables
+	ba, err = filter.New(false, &filter.Rules{
+		DoDBs: []string{doSchema},
+		DoTables: []*filter.Table{
+			{Schema: doSchema, Name: "tbl1"},
+			{Schema: doSchema, Name: "tbl2"},
+		},
+	})
+	c.Assert(err, IsNil)
+
+	rows = sqlmock.NewRows([]string{"Database"})
+	s.addRowsForSchemas(rows, schemas)
+	mock.ExpectQuery(`SHOW DATABASES`).WillReturnRows(rows)
+	rows = sqlmock.NewRows([]string{fmt.Sprintf("Tables_in_%s", doSchema), "Table_type"})
+	s.addRowsForTables(rows, tables)
+	mock.ExpectQuery(fmt.Sprintf("SHOW FULL TABLES IN `%s` WHERE Table_Type != 'VIEW'", doSchema)).WillReturnRows(rows)
+	got, err = FetchAllDoTables(db, ba)
+	c.Assert(err, IsNil)
+	c.Assert(got, HasLen, 1)
+	c.Assert(got[doSchema], DeepEquals, []string{"tbl1", "tbl2"})
+	mock.ExpectationsWereMet()
+}
+
+func (s *testCommonSuite) addRowsForSchemas(rows *sqlmock.Rows, schemas []string) {
+	for _, d := range schemas {
+		rows.AddRow(d)
+	}
+}
+
+func (s *testCommonSuite) addRowsForTables(rows *sqlmock.Rows, tables []string) {
+	for _, table := range tables {
+		rows.AddRow(table, "BASE TABLE")
 	}
 }
