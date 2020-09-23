5ca81daeb675d134b66f8eed21185668b4e9b7e5
Xuecheng Zhang <csuzhangxc@gmail.com>
2020-09-23 15:45:32 +0800

    *: run chaos-mesh operations with GitHub action (#930) (#1073)

---
+++ chaos/cases/source.go
@@ -0,0 +1,73 @@
+// Copyright 2020 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+
+	config2 "github.com/pingcap/dm/dm/config"
+	"github.com/pingcap/dm/dm/pb"
+)
+
+// createSources does `operate-source create` operation for two sources.
+// NOTE: we put two source config files (`source1.yaml` and `source2.yaml`) in `conf` directory.
+func createSources(ctx context.Context, cli pb.MasterClient, cfg *config) error {
+	s1Path := filepath.Join(cfg.ConfigDir, "source1.yaml")
+	s2Path := filepath.Join(cfg.ConfigDir, "source2.yaml")
+
+	s1Content, err := ioutil.ReadFile(s1Path)
+	if err != nil {
+		return err
+	}
+	s2Content, err := ioutil.ReadFile(s2Path)
+	if err != nil {
+		return err
+	}
+
+	var cfg1 config2.SourceConfig
+	var cfg2 config2.SourceConfig
+	if err = cfg1.ParseYaml(string(s1Content)); err != nil {
+		return err
+	}
+	if err = cfg2.ParseYaml(string(s2Content)); err != nil {
+		return err
+	}
+
+	// replace DB config.
+	cfg1.From = cfg.Source1
+	cfg2.From = cfg.Source2
+	s1Content2, err := cfg1.Yaml()
+	if err != nil {
+		return err
+	}
+	s2Content2, err := cfg2.Yaml()
+	if err != nil {
+		return err
+	}
+
+	resp, err := cli.OperateSource(ctx, &pb.OperateSourceRequest{
+		Op:     pb.SourceOp_StartSource,
+		Config: []string{s1Content2, s2Content2},
+	})
+	if err != nil {
+		return err
+	} else if !resp.Result && !strings.Contains(resp.Msg, "already exists") { // imprecise match
+		return fmt.Errorf("fail to create source: %s", resp.Msg)
+	}
+	return nil
+}
