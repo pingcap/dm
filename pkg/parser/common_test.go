84b12c5fce5d8fddc985909339a837dda7aaa4bf
Xuecheng Zhang <csuzhangxc@gmail.com>
2020-09-28 14:20:25 +0800

    *: update parser to support `GRANT CREATE TABLESPACE` syntax (#1048)

--- pkg/parser/common_test.go
+++ pkg/parser/common_test.go
@@ -69,6 +69,10 @@ var sqls = []string{
 	"alter database `test` charset utf8mb4",
 }
 
+var nonDDLs = []string{
+	"GRANT CREATE TABLESPACE ON *.* TO `root`@`%` WITH GRANT OPTION",
+}
+
 func TestSuite(t *testing.T) {
 	TestingT(t)
 }
@@ -80,6 +84,12 @@ func (t *testParserSuite) TestParser(c *C) {
 	p := parser.New()
 
 	for _, sql := range sqls {
+		stmts, err := Parse(p, sql, "", "")
+		c.Assert(err, IsNil)
+		c.Assert(stmts, HasLen, 1)
+	}
+
+	for _, sql := range nonDDLs {
 		stmts, err := Parse(p, sql, "", "")
 		c.Assert(err, IsNil)
 		c.Assert(stmts, HasLen, 1)
