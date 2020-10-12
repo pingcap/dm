63af700e2d530b8ff3388da6515093f8fb2ed9d3
lihaowei <haoweili35@gmail.com>
2020-10-12 21:26:31 +0800

    Fix for issue#1136
    Signed-off-by: lihaowei <haoweili35@gmail.com>

--- tests/dm_syncer/data/db2.prepare.sql
+++ tests/dm_syncer/data/db2.prepare.sql
@@ -3,3 +3,8 @@ create database `dm_syncer`;
 use `dm_syncer`;
 create table t2 (id int auto_increment, name varchar(20), primary key (`id`));
 insert into t2 (name) values ('Arya'), ('Bran'), ('Sansa');
+
+create table dm_syncer_filter_rule(id int auto_increment, name varchar(20), primary key (`id`));
+insert into dm_syncer_filter_rule(name) values ('howie', 'Howie');
+update dm_syncer_filter_rule set id = 1 where name = 'howie';
+drop table dm_syncer_filter_rule;
