a3c477b0f9b7e52d8d87290384e15f61a392a471
lihaowei <haoweili35@gmail.com>
2020-10-13 21:49:56 +0800

    change yaml and toml
    Signed-off-by: lihaowei <haoweili35@gmail.com>

--- tests/dm_syncer/data/db2.prepare.sql
+++ tests/dm_syncer/data/db2.prepare.sql
@@ -5,6 +5,5 @@ create table t2 (id int auto_increment, name varchar(20), primary key (`id`));
 insert into t2 (name) values ('Arya'), ('Bran'), ('Sansa');
 
 create table dm_syncer_filter_rule(id int auto_increment, name varchar(20), primary key (`id`));
-insert into dm_syncer_filter_rule(name) values ('howie', 'Howie');
+insert into dm_syncer_filter_rule(name) values ('howie');
 update dm_syncer_filter_rule set id = 1 where name = 'howie';
-drop table dm_syncer_filter_rule;
