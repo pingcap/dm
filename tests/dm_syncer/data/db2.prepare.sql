85a69057466dcd64511f41ea41437c593585cfee
lihaowei <haoweili35@gmail.com>
2020-10-15 19:53:04 +0800

    remove auto-increment

--- tests/dm_syncer/data/db2.prepare.sql
+++ tests/dm_syncer/data/db2.prepare.sql
@@ -4,11 +4,11 @@ use `dm_syncer`;
 create table t2 (id int auto_increment, name varchar(20), primary key (`id`));
 insert into t2 (name) values ('Arya'), ('Bran'), ('Sansa');
 
-create table dm_syncer_route_rules_1 (id int auto_increment, name varchar(20), primary key (`id`));
+create table dm_syncer_route_rules_1 (id int, name varchar(20), primary key (`id`));
 insert into dm_syncer_route_rules_1 (name) values ('Howie'), ('howie');
 
-create table dm_syncer_route_rules_2 (id int auto_increment, name varchar(20), primary key (`id`));
+create table dm_syncer_route_rules_2 (id int, name varchar(20), primary key (`id`));
 insert into dm_syncer_route_rules_2 (name) values ('howie'), ('Bran'), ('Sansa');
 
-create table dm_syncer_route_rules_3 (id int auto_increment, name varchar(20), primary key (`id`));
+create table dm_syncer_route_rules_3 (id int, name varchar(20), primary key (`id`));
 insert into dm_syncer_route_rules_3 (name) values ('Arya'), ('Howie'), ('Sansa');
