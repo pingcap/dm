25c2b4b66ec84f649bd231a2058b24a5927c47eb
lihaowei <haoweili35@gmail.com>
2020-10-16 20:30:35 +0800

    fix: change key name

--- tests/dm_syncer/data/db2.prepare.sql
+++ tests/dm_syncer/data/db2.prepare.sql
@@ -4,11 +4,11 @@ use `dm_syncer`;
 create table t2 (id int auto_increment, name varchar(20), primary key (`id`));
 insert into t2 (name) values ('Arya'), ('Bran'), ('Sansa');
 
-create table dm_syncer_route_rules_1 (id int, name varchar(20), primary key (`id`));
-insert into dm_syncer_route_rules_1 (id, name) values (1, 'Howie'), (2, 'howie');
+create table dm_syncer_route_rules_1 (id1 int, name varchar(20), primary key (`id1`));
+insert into dm_syncer_route_rules_1 (id1, name) values (1, 'Howie'), (2, 'howie');
 
-create table dm_syncer_route_rules_2 (id int, name varchar(20), primary key (`id`));
-insert into dm_syncer_route_rules_2 (id, name) values (2, 'howie'), (3, 'Bran'), (4, 'Sansa');
+create table dm_syncer_route_rules_2 (id2 int, name varchar(20), primary key (`id2`));
+insert into dm_syncer_route_rules_2 (id2, name) values (2, 'howie'), (3, 'Bran'), (4, 'Sansa');
 
-create table dm_syncer_route_rules_3 (id int, name varchar(20), primary key (`id`));
-insert into dm_syncer_route_rules_3 (id, name) values (3, 'Arya'), (4, 'Howie'), (5, 'Sansa');
+create table dm_syncer_route_rules_3 (id3 int, name varchar(20), primary key (`id3`));
+insert into dm_syncer_route_rules_3 (id3, name) values (3, 'Arya'), (4, 'Howie'), (5, 'Sansa');
