b7286eb2c2a32c43c22214e84a79c33dfd61721a
Howie <haoweili35@gmail.com>
2020-10-19 10:25:17 +0800

    dm-syncer support route-rules (#1150)
    Signed-off-by: lihaowei <haoweili35@gmail.com>

--- tests/dm_syncer/data/db2.prepare.sql
+++ tests/dm_syncer/data/db2.prepare.sql
@@ -3,3 +3,12 @@ create database `dm_syncer`;
 use `dm_syncer`;
 create table t2 (id int auto_increment, name varchar(20), primary key (`id`));
 insert into t2 (name) values ('Arya'), ('Bran'), ('Sansa');
+
+create table dm_syncer_route_rules_1 (id int, name varchar(20), primary key (`id`));
+insert into dm_syncer_route_rules_1 (id, name) values (1, 'Howie'), (2, 'howie');
+
+create table dm_syncer_route_rules_2 (id int, name varchar(20), primary key (`id`));
+insert into dm_syncer_route_rules_2 (id, name) values (3, 'howie'), (4, 'Bran'), (5, 'Sansa');
+
+create table dm_syncer_route_rules_3 (id int, name varchar(20), primary key (`id`));
+insert into dm_syncer_route_rules_3 (id, name) values (6, 'Arya'), (7, 'Howie'), (8, 'Sansa');
