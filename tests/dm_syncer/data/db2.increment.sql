b40dc7a8dba8a70acbdfbd2b597853d3b78532af
lihaowei <haoweili35@gmail.com>
2020-10-20 09:09:35 +0800

    remove unrelated
    Signed-off-by: lihaowei <haoweili35@gmail.com>

--- tests/dm_syncer/data/db2.increment.sql
+++ tests/dm_syncer/data/db2.increment.sql
@@ -1,2 +1,6 @@
 use dm_syncer_plus;
 delete from t2 where name = 'Sansa';
+
+insert into dm_syncer_filter_rule(name) values ('howie');
+update dm_syncer_filter_rule set id = 1 where name = 'howie';
+drop database if exists `dm_syncer_plus`;
--- tests/dm_syncer/run.sh
+++ tests/dm_syncer/run.sh
@@ -13,9 +13,6 @@ function run() {
     run_sql_file $cur/data/db2.prepare.sql $MYSQL_HOST2 $MYSQL_PORT2 $MYSQL_PASSWORD2
     check_contains 'Query OK, 3 rows affected'
 
-    run_sql "show databases;" $TIDB_PORT $TIDB_PASSWORD
-    check_not_contains "dm_syncer_plus"
-
     run_dm_master $WORK_DIR/master $MASTER_PORT $cur/conf/dm-master.toml
     check_rpc_alive $cur/../bin/check_master_online 127.0.0.1:$MASTER_PORT
     run_dm_worker $WORK_DIR/worker1 $WORKER1_PORT $cur/conf/dm-worker1.toml
@@ -41,6 +38,8 @@ function run() {
     run_sql_file $cur/data/db1.increment.sql $MYSQL_HOST1 $MYSQL_PORT1 $MYSQL_PASSWORD1
     run_sql_file $cur/data/db2.increment.sql $MYSQL_HOST2 $MYSQL_PORT2 $MYSQL_PASSWORD2
 
+    run_sql "show databases;" $TIDB_PORT $TIDB_PASSWORD
+    check_not_contains "dm_syncer_plus"
     # start a task in `incremental` mode
     # using account with limited privileges
     kill_dm_worker
