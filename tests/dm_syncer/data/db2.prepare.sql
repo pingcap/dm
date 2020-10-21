b5664c8588212db3265d83c8aa3a1216fe09046d
lihaowei <haoweili35@gmail.com>
2020-10-21 10:02:01 +0800

    change  prepare script
    Signed-off-by: lihaowei <haoweili35@gmail.com>

--- tests/dm_syncer/data/db2.prepare.sql
+++ tests/dm_syncer/data/db2.prepare.sql
@@ -5,6 +5,3 @@ create table t2 (id int auto_increment, name varchar(20), primary key (`id`));
 insert into t2 (name) values ('Arya'), ('Bran'), ('Sansa');
 
 create table dm_syncer_filter_rule(id int auto_increment, name varchar(20), primary key (`id`));
-insert into dm_syncer_filter_rule(name) values ('howie');
-update dm_syncer_filter_rule set id = 1 where name = 'howie';
-drop database if exists `dm_syncer_plus`;
