9e25d980a893d10175a268479e118ea77fae3217
lance6716 <lance6716@gmail.com>
2020-10-27 09:54:59 +0800

    address comment

--- pkg/election/election.go
+++ pkg/election/election.go
@@ -361,12 +361,12 @@ func (e *Election) watchLeader(ctx context.Context, session *concurrency.Session
 	e.campaignMu.Unlock()
 
 	defer func() {
-		e.l.Info("will try resign leader")
+		e.l.Debug("will try resign leader")
 		timeoutCtx, cancel := context.WithTimeout(context.Background(), time.Duration(e.sessionTTL)*time.Second)
 		if err := elec.Resign(timeoutCtx); err != nil {
 			e.l.Warn("fail to resign leader", zap.Stringer("current member", e.info), zap.Error(err))
 		} else {
-			e.l.Info("finish resign leader")
+			e.l.Debug("finish resign leader")
 		}
 		cancel()
 		e.campaignMu.Lock()
