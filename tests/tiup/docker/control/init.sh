5bb0de4fad00ec22a57f51ac8f6ad3e102d2c5aa
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2020-10-29 17:15:11 +0800

    tests: upgrade DM from 1.0 and 2.0 RC to 2.0 via TiUP (#1228) (#1240)
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>
    
    Co-authored-by: Xuecheng Zhang <csuzhangxc@gmail.com>

---
+++ tests/tiup/docker/control/init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ ! -f ~/.ssh/known_hosts ]; then
+    mkdir -m 700 ~/.ssh
+    echo $SSH_PRIVATE_KEY | perl -p -e 's/↩/\n/g' > ~/.ssh/id_rsa
+    chmod 600 ~/.ssh/id_rsa
+    echo $SSH_PUBLIC_KEY > ~/.ssh/id_rsa.pub
+    echo > ~/.ssh/known_hosts
+    for f in $(seq 1 5);do
+	ssh-keyscan -t rsa n$f >> ~/.ssh/known_hosts
+    done
+fi
+
+# hack for keep this container running
+tail -f /dev/null
