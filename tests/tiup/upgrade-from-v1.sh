78b345b13b00c8f89dc03682bf268877703c838a
csuzhangxc <csuzhangxc@gmail.com>
2020-10-29 11:46:48 +0800

    *: refine code

--- tests/tiup/upgrade-from-v1.sh
+++ tests/tiup/upgrade-from-v1.sh
@@ -2,34 +2,13 @@
 
 set -eu
 
-export TEST_DIR=/tmp/dm_test
-export TEST_NAME="upgrade-via-tiup"
-
-WORK_DIR=$TEST_DIR/$TEST_NAME
-mkdir -p $WORK_DIR
+CLUSTER_NAME="dm-v1"
+DM_V2_VER="nightly"
 
 CUR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 PATH=$CUR/../_utils:$PATH # for sync_diff_inspector
 
-TASK_NAME="upgrade_via_tiup"
-CLUSTER_NAME="dm-v1"
-DM_V2_VER="nightly"
-
-DB1=sharding1
-DB2=sharding2
-TBL1=t1
-TBL2=t2
-TBL3=t3
-
-function exec_sql() {
-    echo $3 | mysql -h $1 -P $2
-}
-
-function install_sync_diff() {
-    curl http://download.pingcap.org/tidb-enterprise-tools-latest-linux-amd64.tar.gz | tar xz
-    mkdir -p bin
-    mv tidb-enterprise-tools-latest-linux-amd64/bin/sync_diff_inspector bin/
-}
+source $CUR/lib.sh
 
 # ref https://docs.pingcap.com/zh/tidb-data-migration/stable/deploy-a-dm-cluster-using-ansible
 # script following the docs to deploy dm 1.0.6 using ./ansible_data/inventory.ini
@@ -99,35 +78,13 @@ EOF
 }
 
 function migrate_in_v1 {
-    # drop previous data
-    exec_sql mysql1 3306 "DROP DATABASE IF EXISTS $DB1;" 
-    exec_sql mysql2 3306 "DROP DATABASE IF EXISTS $DB2;" 
-    exec_sql tidb 4000 "DROP DATABASE IF EXISTS db_target;"
-    exec_sql tidb 4000 "DROP DATABASE IF EXISTS dm_meta;"
+    exec_full_stage
 
-    # # prepare full data
-    exec_sql mysql1 3306 "CREATE DATABASE $DB1;"
-    exec_sql mysql2 3306 "CREATE DATABASE $DB2;"
-    exec_sql mysql1 3306 "CREATE TABLE $DB1.$TBL1 (c1 INT PRIMARY KEY, c2 TEXT);"
-    exec_sql mysql1 3306 "CREATE TABLE $DB1.$TBL2 (c1 INT PRIMARY KEY, c2 TEXT);"
-    exec_sql mysql2 3306 "CREATE TABLE $DB2.$TBL2 (c1 INT PRIMARY KEY, c2 TEXT);"
-    exec_sql mysql2 3306 "CREATE TABLE $DB2.$TBL3 (c1 INT PRIMARY KEY, c2 TEXT);"
-
-    exec_sql mysql1 3306 "INSERT INTO $DB1.$TBL1 (c1, c2) VALUES (1, '1');"
-    exec_sql mysql1 3306 "INSERT INTO $DB1.$TBL2 (c1, c2) VALUES (2, '2');"
-    exec_sql mysql2 3306 "INSERT INTO $DB2.$TBL2 (c1, c2) VALUES (11, '11');"
-    exec_sql mysql2 3306 "INSERT INTO $DB2.$TBL3 (c1, c2) VALUES (12, '12');"
-
-    # # start v1 task
+    # start v1 task
     /home/tidb/dm-ansible/dmctl/dmctl --master-addr=master1:8261 start-task $CUR/conf/task.yaml
 
-    # prepare incremental data
-    exec_sql mysql1 3306 "INSERT INTO $DB1.$TBL1 (c1, c2) VALUES (101, '101');"
-    exec_sql mysql1 3306 "INSERT INTO $DB1.$TBL2 (c1, c2) VALUES (102, '102');"
-    exec_sql mysql2 3306 "INSERT INTO $DB2.$TBL2 (c1, c2) VALUES (111, '111');"
-    exec_sql mysql2 3306 "INSERT INTO $DB2.$TBL3 (c1, c2) VALUES (112, '112');"
+    exec_incremental_stage1
 
-    # check data
     check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
 }
 
@@ -146,13 +103,8 @@ function import_to_v2_by_tiup() {
 }
 
 function migrate_in_v2 {
-    # prepare incremental data
-    exec_sql mysql1 3306 "INSERT INTO $DB1.$TBL1 (c1, c2) VALUES (201, '201');"
-    exec_sql mysql1 3306 "INSERT INTO $DB1.$TBL2 (c1, c2) VALUES (202, '202');"
-    exec_sql mysql2 3306 "INSERT INTO $DB2.$TBL2 (c1, c2) VALUES (211, '211');"
-    exec_sql mysql2 3306 "INSERT INTO $DB2.$TBL3 (c1, c2) VALUES (212, '212');"
+    exec_incremental_stage2
 
-    # check data
     check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
 
     # stop v2 task
