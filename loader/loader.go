dd8c11a0493ba4e3df88f00e2c27816fe4fa1bcc
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2020-11-02 17:33:17 +0800

    fix: support "sql" prefix exists in table name (#1256) (#1259)
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>
    
    Co-authored-by: Xuecheng Zhang <csuzhangxc@gmail.com>

--- loader/loader.go
+++ loader/loader.go
@@ -868,7 +868,7 @@ func (l *Loader) prepareDbFiles(files map[string]struct{}) error {
 			continue
 		}
 
-		idx := strings.Index(file, "-schema-create.sql")
+		idx := strings.LastIndex(file, "-schema-create.sql")
 		if idx > 0 {
 			schemaFileCount++
 			db := file[:idx]
@@ -900,7 +900,7 @@ func (l *Loader) prepareTableFiles(files map[string]struct{}) error {
 			continue
 		}
 
-		idx := strings.Index(file, "-schema.sql")
+		idx := strings.LastIndex(file, "-schema.sql")
 		name := file[:idx]
 		fields := strings.Split(name, ".")
 		if len(fields) != 2 {
