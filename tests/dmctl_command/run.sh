76f338d5627a6981da1c50395993925f05e84db3
gmhdbjd <gmhdbjd@gmail.com>
2020-12-02 11:53:43 +0800

    fix default relay-dir

--- tests/dmctl_command/run.sh
+++ tests/dmctl_command/run.sh
@@ -93,7 +93,6 @@ function run() {
     cp $cur/conf/source1.yaml $WORK_DIR/source1.yaml
     cp $cur/conf/source2.yaml $WORK_DIR/source2.yaml
     sed -i "/relay-binlog-name/i\relay-dir: $WORK_DIR/worker1/relay_log" $WORK_DIR/source1.yaml
-    sed -i "/relay-binlog-name/i\relay-dir: $WORK_DIR/worker2/relay_log" $WORK_DIR/source2.yaml
     dmctl_operate_source create $WORK_DIR/source1.yaml $SOURCE_ID1
     dmctl_operate_source create $WORK_DIR/source2.yaml $SOURCE_ID2
 
