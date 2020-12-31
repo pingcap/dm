9348a6cce91d03e7ffe3ab7d5ee1747ec01e2c96
gmhdbjd <gmhdbjd@gmail.com>
2020-12-31 13:57:23 +0800

    add test case for upstream database switch

--- tests/upstream_switch/case.sh
+++ tests/upstream_switch/case.sh
@@ -2,6 +2,206 @@
 
 set -eu
 
-echo "hello world"
+CUR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PATH=$CUR/../_utils:$PATH # for sync_diff_inspector
 
-touch /tmp/log.log
+source $CUR/lib.sh
+
+function clean_data() {
+    echo "-------clean_data--------"
+
+    exec_sql $master_57_host "stop slave;"
+    exec_sql $slave_57_host "stop slave;"
+    exec_sql $master_8_host "stop slave;"
+    exec_sql $slave_8_host "stop slave;"
+
+    exec_sql $master_57_host "drop database if exists db1;"
+    exec_sql $master_57_host "drop database if exists db2;"
+    exec_sql $master_57_host "drop database if exists ${db};"
+    exec_sql $master_57_host "reset master;"
+    exec_sql $slave_57_host "drop database if exists db1;"
+    exec_sql $slave_57_host "drop database if exists db2;"
+    exec_sql $slave_57_host "drop database if exists ${db};"
+    exec_sql $slave_57_host "reset master;"
+    exec_sql $master_8_host "drop database if exists db1;"
+    exec_sql $master_8_host "drop database if exists db2;"
+    exec_sql $master_8_host "drop database if exists ${db};"
+    exec_sql $master_8_host "reset master;"
+    exec_sql $slave_8_host "drop database if exists db1;"
+    exec_sql $slave_8_host "drop database if exists db2;"
+    exec_sql $slave_8_host "drop database if exists ${db};"
+    exec_sql $slave_8_host "reset master;"
+    exec_tidb $tidb_host "drop database if exists db1;"
+    exec_tidb $tidb_host "drop database if exists db2;"
+    exec_tidb $tidb_host "drop database if exists ${db};"
+}
+
+function prepare_binlogs() {
+    echo "-------prepare_binlogs--------"
+
+    prepare_more_binlogs $master_57_host
+    prepare_less_binlogs $slave_57_host
+    prepare_less_binlogs $master_8_host
+    prepare_more_binlogs $slave_8_host
+}
+
+function setup_replica() {
+    echo "-------setup_replica--------"
+
+    master_57_status=($(get_master_status $master_57_host))
+    slave_57_status=($(get_master_status $slave_57_host))
+    master_8_status=($(get_master_status $master_8_host))
+    slave_8_status=($(get_master_status $slave_8_host))
+    echo "master_57_status" ${master_57_status[@]}
+    echo "slave_57_status" ${slave_57_status[@]}
+    echo "master_8_status" ${master_8_status[@]}
+    echo "slave_8_status" ${slave_8_status[@]}
+
+    # master <-- slave
+    change_master_to_pos $slave_57_host $master_57_host ${master_57_status[0]} ${master_57_status[1]}
+
+    # master <--> master
+    change_master_to_gtid $slave_8_host $master_8_host
+    change_master_to_gtid $master_8_host $slave_8_host
+}
+
+function run_dm_components() {
+    echo "-------run_dm_components--------"
+
+    pkill -9 dm-master || true
+    pkill -9 dm-worker || true
+
+    run_dm_master $WORK_DIR/master $MASTER_PORT $CUR/conf/dm-master.toml
+    run_dm_ctl_with_retry $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+        "list-member" \
+        "alive" 1
+
+    run_dm_worker $WORK_DIR/worker1 $WORKER1_PORT $CUR/conf/dm-worker1.toml
+    run_dm_worker $WORK_DIR/worker2 $WORKER2_PORT $CUR/conf/dm-worker2.toml
+    run_dm_ctl_with_retry $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+        "list-member" \
+        "alive" 1 \
+        "free" 2
+}
+
+function create_sources() {
+    echo "-------create_sources--------"
+
+    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+        "operate-source create $CUR/conf/source1.yaml" \
+        "\"result\": true" 2
+    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+        "operate-source create $CUR/conf/source2.yaml" \
+        "\"result\": true" 2
+}
+
+function gen_full_data() {
+    echo "-------gen_full_data--------"
+
+    exec_sql $host1 "create database ${db};"
+    exec_sql $host1 "create table ${db}.${tb}(id int primary key, a int);"
+    for i in $(seq 1 1000); do
+        exec_sql $host1 "insert into ${db}.${tb} values($i,$i);"
+    done
+
+    exec_sql $host2 "create database ${db};"
+    exec_sql $host2 "create table ${db}.${tb}(id int primary key, a int);"
+    for i in $(seq 1001 2000); do
+        exec_sql $host2 "insert into ${db}.${tb} values($i,$i);"
+    done
+}
+
+
+function gen_incr_data() {
+    echo "-------gen_incr_data--------"
+
+    for i in $(seq 2001 2500); do
+        exec_sql $host1 "insert into ${db}.${tb} values($i,$i);"
+    done
+    for i in $(seq 2501 3000); do
+        exec_sql $host2 "insert into ${db}.${tb} values($i,$i);"
+    done
+    exec_sql $host1 "alter table ${db}.${tb} add column b int;"
+    exec_sql $host2 "alter table ${db}.${tb} add column b int;"
+    for i in $(seq 3001 3500); do
+        exec_sql $host1 "insert into ${db}.${tb} values($i,$i,$i);"
+    done
+    for i in $(seq 3501 4000); do
+        exec_sql $host2 "insert into ${db}.${tb} values($i,$i,$i);"
+    done
+
+    docker-compose -f $CUR/docker-compose.yml pause mysql57_master
+    docker-compose -f $CUR/docker-compose.yml pause mysql8_master
+    wait_mysql $host1 2
+    wait_mysql $host2 2
+
+    for i in $(seq 4001 4500); do
+        exec_sql $host1 "insert into ${db}.${tb} values($i,$i,$i);"
+    done
+    for i in $(seq 4501 5000); do
+        exec_sql $host2 "insert into ${db}.${tb} values($i,$i,$i);"
+    done
+    exec_sql $host1 "alter table ${db}.${tb} add column c int;"
+    exec_sql $host2 "alter table ${db}.${tb} add column c int;"
+
+    docker-compose -f $CUR/docker-compose.yml unpause mysql8_master
+    wait_mysql $host2 1
+
+    for i in $(seq 5001 5500); do
+        exec_sql $host1 "insert into ${db}.${tb} values($i,$i,$i,$i);"
+    done
+    for i in $(seq 5501 6000); do
+        exec_sql $host2 "insert into ${db}.${tb} values($i,$i,$i,$i);"
+    done
+}
+
+function start_task() {
+    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+        "start-task $CUR/conf/task-pessimistic.yaml --remove-meta" \
+        "\result\": true" 3
+}
+
+function verify_result() {
+    check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
+    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+        "query-status task_pessimistic -s mysql-replica-02" \
+        "relaySubDir.*000003" 1
+}
+
+function clean_task() {
+    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+        "stop-task task_pessimistic" \
+        "\result\": true" 3
+    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+        "pause-relay -s mysql-replica-02" \
+        "\result\": true" 2
+    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+        "operate-source stop mysql-replica-01" \
+        "\result\": true" 2
+    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+        "operate-source stop mysql-replica-02" \
+        "\result\": true" 2
+    docker-compose -f $CUR/docker-compose.yml unpause mysql57_master
+}
+
+function check_master() {
+    wait_mysql $host1 1
+    wait_mysql $host2 1
+}
+
+function test() {
+    check_master
+    install_sync_diff
+    clean_data
+    prepare_binlogs
+    setup_replica
+    gen_full_data
+    run_dm_components
+    create_sources
+    start_task
+    gen_incr_data
+    verify_result
+    clean_task
+}
+
+test
---
+++ tests/upstream_switch/lib.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+set -eu
+
+export TEST_DIR=/tmp/dm_test
+export TEST_NAME="upstream-switch"
+
+WORK_DIR=$TEST_DIR/$TEST_NAME
+rm -rf $WORK_DIR
+mkdir -p $WORK_DIR
+
+db="db_pessimistic"
+tb="tb"
+host1="172.28.128.2"
+host2="172.28.128.3"
+master_57_host="172.28.128.4"
+slave_57_host="172.28.128.5"
+master_8_host="172.28.128.6"
+slave_8_host="172.28.128.7"
+tidb_host="172.28.128.8"
+MASTER_PORT=8261
+WORKER1_PORT=8262
+WORKER2_PORT=8263
+
+function exec_sql() {
+    echo $2 | mysql -uroot -h$1 -P3306 -p123456
+}
+
+function exec_tidb() {
+    echo $2 | mysql -uroot -h$1 -P4000
+}
+
+function install_sync_diff() {
+    curl http://download.pingcap.org/tidb-enterprise-tools-latest-linux-amd64.tar.gz | tar xz
+    mkdir -p bin
+    mv tidb-enterprise-tools-latest-linux-amd64/bin/sync_diff_inspector bin/
+}
+
+
+function prepare_more_binlogs() {
+    exec_sql $1 "create database db1;"
+    exec_sql $1 "flush logs;"
+    exec_sql $1 "create table db1.tb1(id int);"
+    exec_sql $1 "flush logs;"
+    exec_sql $1 "insert into db1.tb1 values(1);"
+    exec_sql $1 "insert into db1.tb1 values(2),(3),(4);"
+}
+
+function prepare_less_binlogs() {
+    exec_sql $1 "create database db2;"
+    exec_sql $1 "flush logs;"
+    exec_sql $1 "create table db2.tb2(id int);"
+    exec_sql $1 "insert into db2.tb2 values(1);"
+    exec_sql $1 "insert into db2.tb2 values(2),(3);"
+}
+
+
+function get_master_status() {
+    arr=$(echo "show master status;" | mysql -uroot -h$1 -P3306 -p123456 | awk 'NR==2' )
+    echo $arr
+}
+
+function change_master_to_pos() {
+    exec_sql $1 "stop slave;"
+    echo "change master to master_host='$2',master_user='root',master_password='123456',master_log_file='$3',master_log_pos=$4;"
+    exec_sql $1 "change master to master_host='$2',master_user='root',master_password='123456',master_log_file='$3',master_log_pos=$4;"
+    exec_sql $1 "start slave;"
+}
+
+function change_master_to_gtid() {
+    exec_sql $1 "stop slave;"
+    exec_sql $1 "change master to master_host='$2',master_user='root',master_password='123456',master_auto_position=1;"
+    exec_sql $1 "start slave;"
+}
+
+function wait_mysql() {
+    echo "-------wait_mysql--------"
+
+    i=0
+    while ! mysqladmin -h$1 -P3306 -uroot ping --connect-timeout=1 > /dev/null 2>&1 ; do
+        echo "wait mysql"
+        i=$((i+1))
+        if [ "$i" -gt 20 ]; then
+            echo "wait for mysql $1:3306 timeout"
+            exit 1
+        fi
+        sleep 1
+    done
+    server_id=$(echo "show variables like 'server_id';" | mysql -uroot -h$1 -P3306 -p123456 | awk 'NR==2' | awk  '{print $2}')
+    if [ "$server_id" != $2 ]; then
+        echo "different server_id: " $server_id ", expect: " $2
+        exit 1
+    fi
+}
