a0af040d1cecaa2672cd4a531fc29a5646a45c33
zeminzhou <50103576+zeminzhou@users.noreply.github.com>
2021-02-02 11:26:00 +0800

    config: add warning, when users set configuration to mydumper/loader/syncer b… (#1410)

--- pkg/terror/error_list.go
+++ pkg/terror/error_list.go
@@ -226,6 +226,7 @@ const (
 	codeConfigEtcdParse
 	codeConfigMissingForBound
 	codeConfigBinlogEventFilter
+	codeConfigGlobalConfigsUnused
 )
 
 // Binlog operation error code list
@@ -823,6 +824,7 @@ var (
 	ErrConfigEtcdParse              = New(codeConfigEtcdParse, ClassConfig, ScopeInternal, LevelHigh, "incapable config of %s from etcd", "")
 	ErrConfigMissingForBound        = New(codeConfigMissingForBound, ClassConfig, ScopeInternal, LevelHigh, "source bound %s doesn't have related source config in etcd", "")
 	ErrConfigBinlogEventFilter      = New(codeConfigBinlogEventFilter, ClassConfig, ScopeInternal, LevelHigh, "generate binlog event filter", "Please check the `filters` config in source and task configuration files.")
+	ErrConfigGlobalConfigsUnused    = New(codeConfigGlobalConfigsUnused, ClassConfig, ScopeInternal, LevelHigh, "The configurations as following %v are set in global configuration but instances don't use them", "Please check the configuration files.")
 
 	// Binlog operation error
 	ErrBinlogExtractPosition = New(codeBinlogExtractPosition, ClassBinlogOp, ScopeInternal, LevelHigh, "", "")
