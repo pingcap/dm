5edd1ccdf2de2998db4aff2a230f48d40583a35d
hidehalo <tianchen_cc@yeah.net>
2021-02-09 17:37:49 +0800

    tests: try to fix CI tests

--- tests/all_mode/data/db1.prepare.sql
+++ tests/all_mode/data/db1.prepare.sql
@@ -1,12 +1,10 @@
 drop database if exists `all_mode`;
 create database `all_mode`;
 use `all_mode`;
-create table t1 (id int NOT NULL AUTO_INCREMENT, name varchar(20), PRIMARY KEY (id));
+create table `t1` (`id` int NOT NULL AUTO_INCREMENT, `name` varchar(20), PRIMARY KEY (`id`));
 -- test ANSI_QUOTES works with quote in string
-insert into t1 (id, name) values (1, 'ar"ya'), (2, 'catelyn');
-
--- test sql_mode=NO_AUTO_VALUE_ON_ZERO
-insert into t1 (id, name) values (0, 'lalala');
+insert into `t1` (`id`, `name`) values (1, 'ar"ya'), (2, 'catelyn');
+insert into `t1` (`id`, `name`) values (0, 'lalala');
 
 -- test block-allow-list
 drop database if exists `ignore_db`;
--- tests/all_mode/data/db2.prepare.sql
+++ tests/all_mode/data/db2.prepare.sql
@@ -1,8 +1,8 @@
 drop database if exists `all_mode`;
 create database `all_mode`;
 use `all_mode`;
-create table t2 (id int auto_increment, name varchar(20), primary key (`id`));
-insert into t2 (name) values ('Arya'), ('Bran'), ('Sansa');
+create table `t2` (`id` int AUTO_INCREMENT, `name` varchar(20), primary key (`id`));
+insert into `t2` (`name`) values ('Arya'), ('Bran'), ('Sansa');
 
 -- test block-allow-list
 drop database if exists `ignore_db`;
