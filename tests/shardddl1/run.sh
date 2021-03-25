8450ef4847f4cdcb446bef2391b13c64bb9562bb
gmhdbjd <gmhdbjd@gmail.com>
2021-03-25 14:41:41 +0800

    revert debug

--- tests/shardddl1/run.sh
+++ tests/shardddl1/run.sh
@@ -656,17 +656,17 @@ function DM_RECOVER_LOCK() {
 function run() {
     init_cluster
     init_database
-#    start=1
-#    end=35
-#    except=(024 025 029)
-#    for i in $(seq -f "%03g" ${start} ${end}); do
-#        if [[ ${except[@]} =~ $i ]]; then
-#            continue
-#        fi
-#        DM_${i}
-#        sleep 1
-#    done
-#    DM_RENAME_COLUMN_OPTIMISTIC
+    start=1
+    end=35
+    except=(024 025 029)
+    for i in $(seq -f "%03g" ${start} ${end}); do
+        if [[ ${except[@]} =~ $i ]]; then
+            continue
+        fi
+        DM_${i}
+        sleep 1
+    done
+    DM_RENAME_COLUMN_OPTIMISTIC
     DM_RECOVER_LOCK
 }
 
