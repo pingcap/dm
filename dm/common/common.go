6b0aafdffe9cd308e3194b4cc8992c46f8fe660c
lance6716 <lance6716@gmail.com>
2021-03-29 13:32:46 +0800

    common: quote and escape before encoding into etcd key

--- dm/common/common.go
+++ dm/common/common.go
@@ -28,57 +28,57 @@ var (
 	ClusterVersionKey string = "/dm-cluster/version"
 	// WorkerRegisterKeyAdapter is used to encode and decode register key.
 	// k/v: Encode(worker-name) -> the information of the DM-worker node.
-	WorkerRegisterKeyAdapter KeyAdapter = keyHexEncoderDecoder("/dm-worker/r/")
+	WorkerRegisterKeyAdapter KeyAdapter = keyHexEncoderDecoderV2("/dm-worker/r/")
 	// WorkerKeepAliveKeyAdapter is used to encode and decode keepalive key.
 	// k/v: Encode(worker-name) -> time
-	WorkerKeepAliveKeyAdapter KeyAdapter = keyHexEncoderDecoder("/dm-worker/a/")
+	WorkerKeepAliveKeyAdapter KeyAdapter = keyHexEncoderDecoderV2("/dm-worker/a/")
 	// UpstreamConfigKeyAdapter stores all config of which MySQL-task has not stopped.
 	// k/v: Encode(source-id) -> config
-	UpstreamConfigKeyAdapter KeyAdapter = keyEncoderDecoder("/dm-master/upstream/config/")
+	UpstreamConfigKeyAdapter KeyAdapter = keyEncoderDecoderV2("/dm-master/upstream/config/")
 	// UpstreamBoundWorkerKeyAdapter is used to store address of worker in which MySQL-tasks which are running.
 	// k/v: Encode(worker-name) -> the bound relationship.
-	UpstreamBoundWorkerKeyAdapter KeyAdapter = keyHexEncoderDecoder("/dm-master/bound-worker/")
+	UpstreamBoundWorkerKeyAdapter KeyAdapter = keyHexEncoderDecoderV2("/dm-master/bound-worker/")
 	// UpstreamLastBoundWorkerKeyAdapter is used to store address of worker in which MySQL-tasks which are running.
 	// different with UpstreamBoundWorkerKeyAdapter, this kv should not be deleted when unbound, to provide a priority
 	// k/v: Encode(worker-name) -> the bound relationship.
-	UpstreamLastBoundWorkerKeyAdapter KeyAdapter = keyHexEncoderDecoder("/dm-master/last-bound-worker/")
+	UpstreamLastBoundWorkerKeyAdapter KeyAdapter = keyHexEncoderDecoderV2("/dm-master/last-bound-worker/")
 	// UpstreamRelayWorkerKeyAdapter is used to store the upstream which this worker needs to pull relay log
 	// k/v: Encode(worker-name) -> source-id
-	UpstreamRelayWorkerKeyAdapter KeyAdapter = keyHexEncoderDecoder("/dm-master/relay-worker/")
+	UpstreamRelayWorkerKeyAdapter KeyAdapter = keyHexEncoderDecoderV2("/dm-master/relay-worker/")
 	// TaskConfigKeyAdapter is used to store task config string.
 	// k/v: Encode(task-name) -> task-config-string
-	TaskConfigKeyAdapter KeyAdapter = keyHexEncoderDecoder("/dm-master/task/")
+	TaskConfigKeyAdapter KeyAdapter = keyHexEncoderDecoderV2("/dm-master/task/")
 	// UpstreamSubTaskKeyAdapter is used to store SubTask which are subscribing data from MySQL source.
 	// k/v: Encode(source-id, task-name) -> SubTaskConfig
-	UpstreamSubTaskKeyAdapter KeyAdapter = keyHexEncoderDecoder("/dm-master/upstream/subtask/")
+	UpstreamSubTaskKeyAdapter KeyAdapter = keyHexEncoderDecoderV2("/dm-master/upstream/subtask/")
 	// StageRelayKeyAdapter is used to store the running stage of the relay.
 	// k/v: Encode(source-id) -> the running stage of the relay.
-	StageRelayKeyAdapter KeyAdapter = keyEncoderDecoder("/dm-master/stage/relay/")
+	StageRelayKeyAdapter KeyAdapter = keyEncoderDecoderV2("/dm-master/stage/relay/")
 	// StageSubTaskKeyAdapter is used to store the running stage of the subtask.
 	// k/v: Encode(source-id, task-name) -> the running stage of the subtask.
-	StageSubTaskKeyAdapter KeyAdapter = keyHexEncoderDecoder("/dm-master/stage/subtask/")
+	StageSubTaskKeyAdapter KeyAdapter = keyHexEncoderDecoderV2("/dm-master/stage/subtask/")
 
 	// ShardDDLPessimismInfoKeyAdapter is used to store shard DDL info in pessimistic model.
 	// k/v: Encode(task-name, source-id) -> shard DDL info
-	ShardDDLPessimismInfoKeyAdapter KeyAdapter = keyHexEncoderDecoder("/dm-master/shardddl-pessimism/info/")
+	ShardDDLPessimismInfoKeyAdapter KeyAdapter = keyHexEncoderDecoderV2("/dm-master/shardddl-pessimism/info/")
 	// ShardDDLPessimismOperationKeyAdapter is used to store shard DDL operation in pessimistic model.
 	// k/v: Encode(task-name, source-id) -> shard DDL operation
-	ShardDDLPessimismOperationKeyAdapter KeyAdapter = keyHexEncoderDecoder("/dm-master/shardddl-pessimism/operation/")
+	ShardDDLPessimismOperationKeyAdapter KeyAdapter = keyHexEncoderDecoderV2("/dm-master/shardddl-pessimism/operation/")
 
 	// ShardDDLOptimismSourceTablesKeyAdapter is used to store INITIAL upstream schema & table names when starting the subtask.
 	// In other words, if any Info for this subtask exists, we should obey source tables in the Info.
 	// This is because the current upstream tables may not match the tables that the binlog stream has reached.
 	// k/v: Encode(task-name, source-id) -> upstream schema & table names.
-	ShardDDLOptimismSourceTablesKeyAdapter KeyAdapter = keyHexEncoderDecoder("/dm-master/shardddl-optimism/source-tables/")
+	ShardDDLOptimismSourceTablesKeyAdapter KeyAdapter = keyHexEncoderDecoderV2("/dm-master/shardddl-optimism/source-tables/")
 	// ShardDDLOptimismInfoKeyAdapter is used to store shard DDL info in optimistic model.
 	// k/v: Encode(task-name, source-id, upstream-schema-name, upstream-table-name) -> shard DDL info.
-	ShardDDLOptimismInfoKeyAdapter KeyAdapter = keyHexEncoderDecoder("/dm-master/shardddl-optimism/info/")
+	ShardDDLOptimismInfoKeyAdapter KeyAdapter = keyHexEncoderDecoderV2("/dm-master/shardddl-optimism/info/")
 	// ShardDDLOptimismOperationKeyAdapter is used to store shard DDL operation in optimistic model.
 	// k/v: Encode(task-name, source-id, upstream-schema-name, upstream-table-name) -> shard DDL operation.
-	ShardDDLOptimismOperationKeyAdapter KeyAdapter = keyHexEncoderDecoder("/dm-master/shardddl-optimism/operation/")
+	ShardDDLOptimismOperationKeyAdapter KeyAdapter = keyHexEncoderDecoderV2("/dm-master/shardddl-optimism/operation/")
 	// ShardDDLOptimismInitSchemaKeyAdapter is used to store the initial schema (before constructed the lock) of merged tables.
 	// k/v: Encode(task-name, downstream-schema-name, downstream-table-name) -> table schema.
-	ShardDDLOptimismInitSchemaKeyAdapter KeyAdapter = keyHexEncoderDecoder("/dm-master/shardddl-optimism/init-schema/")
+	ShardDDLOptimismInitSchemaKeyAdapter KeyAdapter = keyHexEncoderDecoderV2("/dm-master/shardddl-optimism/init-schema/")
 	// ShardDDLOptimismDroppedColumnsKeyAdapter is used to store the columns that are not fully dropped
 	// k/v: Encode(task-name, downstream-schema-name, downstream-table-name, column-name, source-id, upstream-schema-name, upstream-table-name) -> empty
 	// If we don't identify different upstream tables, we may report an error for tb2 in the following case.
@@ -87,7 +87,7 @@ var (
 	// tb1: +a +b +c           -c
 	// tb2:                       +a +b +c
 	// tb3:          +a +b +c
-	ShardDDLOptimismDroppedColumnsKeyAdapter KeyAdapter = keyHexEncoderDecoder("/dm-master/shardddl-optimism/dropped-columns/")
+	ShardDDLOptimismDroppedColumnsKeyAdapter KeyAdapter = keyHexEncoderDecoderV2("/dm-master/shardddl-optimism/dropped-columns/")
 )
 
 func keyAdapterKeysLen(s KeyAdapter) int {
@@ -171,5 +171,98 @@ func (s keyHexEncoderDecoder) Decode(key string) ([]string, error) {
 }
 
 func (s keyHexEncoderDecoder) Path() string {
+	return string(s)
+}
+
+// version V2 fixed the bug that XXDecoder.Encode("s1") is the prefix of XXDecoder.Encode("s10"), so watching prefix
+// gets unexpected KV
+type keyEncoderDecoderV2 string
+type keyHexEncoderDecoderV2 string
+
+const (
+	quote  = '`'
+	escape = '\\'
+)
+
+func quotes(s string) string {
+	var b strings.Builder
+	b.Grow(2 + len(s))
+	b.WriteByte(quote)
+	for _, c := range s {
+		switch c {
+		case quote, escape:
+			b.WriteByte(escape)
+			b.WriteString(string(c))
+		default:
+			b.WriteString(string(c))
+		}
+	}
+	b.WriteByte(quote)
+	return b.String()
+}
+
+func unquotes(s string) string {
+	var b strings.Builder
+	b.Grow(len(s) - 2)
+	runes := []rune(s)
+	i := 1
+	for i < len(runes)-1 {
+		if runes[i] == escape {
+			i++
+		}
+		b.WriteString(string(runes[i]))
+		i++
+	}
+	return b.String()
+}
+
+func (s keyEncoderDecoderV2) Encode(keys ...string) string {
+	t := []string{string(s)}
+	for _, k := range keys {
+		t = append(t, quotes(k))
+	}
+	return path.Join(t...)
+}
+
+func (s keyEncoderDecoderV2) Decode(key string) ([]string, error) {
+	v := strings.TrimPrefix(key, string(s))
+	vals := strings.Split(v, "/")
+	if l := keyAdapterKeysLen(s); l != len(vals) {
+		return nil, terror.ErrDecodeEtcdKeyFail.Generate(fmt.Sprintf("decoder is %s, the key is %s", string(s), key))
+	}
+	for i, val := range vals {
+		vals[i] = unquotes(val)
+	}
+	return vals, nil
+}
+
+func (s keyEncoderDecoderV2) Path() string {
+	return string(s)
+}
+
+func (s keyHexEncoderDecoderV2) Encode(keys ...string) string {
+	t := []string{string(s)}
+	for _, key := range keys {
+		t = append(t, quotes(hex.EncodeToString([]byte(key))))
+	}
+	return path.Join(t...)
+}
+
+func (s keyHexEncoderDecoderV2) Decode(key string) ([]string, error) {
+	v := strings.Split(strings.TrimPrefix(key, string(s)), "/")
+	if l := keyAdapterKeysLen(s); l != len(v) {
+		return nil, terror.ErrDecodeEtcdKeyFail.Generate(fmt.Sprintf("decoder is %s, the key is %s", string(s), key))
+	}
+	for i, k := range v {
+		dec, err := hex.DecodeString(unquotes(k))
+		if err != nil {
+			return nil, terror.ErrDecodeEtcdKeyFail.Generate(err.Error())
+		}
+		v[i] = string(dec)
+	}
+	return v, nil
+}
+
+func (s keyHexEncoderDecoderV2) Path() string {
 	return string(s)
 }
--- dm/common/common_test.go
+++ dm/common/common_test.go
@@ -28,7 +28,7 @@ type testCommon struct{}
 
 var _ = Suite(&testCommon{})
 
-func (t *testCommon) TestKeyAdapter(c *C) {
+func (t *testCommon) TestKeyAdapterV2(c *C) {
 	testCases := []struct {
 		keys    []string
 		adapter KeyAdapter
@@ -37,37 +37,37 @@ func (t *testCommon) TestKeyAdapter(c *C) {
 		{
 			keys:    []string{"127.0.0.1:2382"},
 			adapter: WorkerRegisterKeyAdapter,
-			want:    "/dm-worker/r/3132372e302e302e313a32333832",
+			want:    "/dm-worker/r/`3132372e302e302e313a32333832`",
 		},
 		{
 			keys:    []string{"worker1"},
 			adapter: WorkerKeepAliveKeyAdapter,
-			want:    "/dm-worker/a/776f726b657231",
+			want:    "/dm-worker/a/`776f726b657231`",
 		},
 		{
 			keys:    []string{"mysql1"},
 			adapter: UpstreamConfigKeyAdapter,
-			want:    "/dm-master/upstream/config/mysql1",
+			want:    "/dm-master/upstream/config/`mysql1`",
 		},
 		{
 			keys:    []string{"127.0.0.1:2382"},
 			adapter: UpstreamBoundWorkerKeyAdapter,
-			want:    "/dm-master/bound-worker/3132372e302e302e313a32333832",
+			want:    "/dm-master/bound-worker/`3132372e302e302e313a32333832`",
 		},
 		{
 			keys:    []string{"mysql1", "test"},
 			adapter: UpstreamSubTaskKeyAdapter,
-			want:    "/dm-master/upstream/subtask/6d7973716c31/74657374",
+			want:    "/dm-master/upstream/subtask/`6d7973716c31`/`74657374`",
 		},
 		{
 			keys:    []string{"test", "target_db", "target_table"},
 			adapter: ShardDDLOptimismInitSchemaKeyAdapter,
-			want:    "/dm-master/shardddl-optimism/init-schema/74657374/7461726765745f6462/7461726765745f7461626c65",
+			want:    "/dm-master/shardddl-optimism/init-schema/`74657374`/`7461726765745f6462`/`7461726765745f7461626c65`",
 		},
 		{
 			keys:    []string{"test", "mysql_replica_01", "target_db", "target_table"},
 			adapter: ShardDDLOptimismInfoKeyAdapter,
-			want:    "/dm-master/shardddl-optimism/info/74657374/6d7973716c5f7265706c6963615f3031/7461726765745f6462/7461726765745f7461626c65",
+			want:    "/dm-master/shardddl-optimism/info/`74657374`/`6d7973716c5f7265706c6963615f3031`/`7461726765745f6462`/`7461726765745f7461626c65`",
 		},
 	}
 
@@ -87,4 +87,36 @@ func (t *testCommon) TestIsErrNetClosing(c *C) {
 	c.Assert(IsErrNetClosing(err), IsFalse)
 	_, err = server.Accept()
 	c.Assert(IsErrNetClosing(err), IsTrue)
+}
+
+func (t *testCommon) TestQuotesUnquotes(c *C) {
+	cases := []struct {
+		input    string
+		expected string
+	}{
+		{
+			"",
+			"``",
+		},
+		{
+			"1",
+			"`1`",
+		},
+		{
+			"`",
+			"`\\``",
+		},
+		{
+			"\\",
+			"`\\\\`",
+		},
+		{
+			"中文1🀄️`\\2",
+			"`中文1🀄️\\`\\\\2`",
+		},
+	}
+	for _, ca := range cases {
+		c.Assert(quotes(ca.input), Equals, ca.expected)
+		c.Assert(unquotes(ca.expected), Equals, ca.input)
+	}
 }
