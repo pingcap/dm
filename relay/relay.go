8222dc8d051aa4cc8e012361e515af4d8ac8c5b4
lance6716 <lance6716@gmail.com>
2021-04-09 17:12:07 +0800

    Update relay/relay.go

--- relay/relay.go
+++ relay/relay.go
@@ -919,7 +919,7 @@ func (r *Relay) Reload(newCfg *Config) error {
 	r.cfg.Charset = newCfg.Charset
 
 	r.closeDB()
-	if r.cfg.From.RawDBCfg != nil {
+	if r.cfg.From.RawDBCfg == nil {
 		r.cfg.From.RawDBCfg = config.DefaultRawDBConfig()
 	}
 	r.cfg.From.RawDBCfg.ReadTimeout = showStatusConnectionTimeout
