41c95891b1545a71af3e4e813b1fbb3734492f7a
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2021-04-09 15:29:00 +0800

    cherry pick #1540 to release-2.0 (#1572)

--- pkg/shardddl/optimism/operation.go
+++ pkg/shardddl/optimism/operation.go
@@ -225,8 +225,15 @@ func GetInfosOperationsByTask(cli *clientv3.Client, task string) ([]Info, []Oper
 func WatchOperationPut(ctx context.Context, cli *clientv3.Client,
 	task, source, upSchema, upTable string, revision int64,
 	outCh chan<- Operation, errCh chan<- error) {
-	ch := cli.Watch(ctx, common.ShardDDLOptimismOperationKeyAdapter.Encode(task, source, upSchema, upTable),
-		clientv3.WithPrefix(), clientv3.WithRev(revision))
+	var ch clientv3.WatchChan
+	// caller may use empty keys to expect a prefix watch
+	if upTable == "" {
+		ch = cli.Watch(ctx, common.ShardDDLOptimismOperationKeyAdapter.Path(), clientv3.WithPrefix(),
+			clientv3.WithRev(revision))
+	} else {
+		ch = cli.Watch(ctx, common.ShardDDLOptimismOperationKeyAdapter.Encode(task, source, upSchema, upTable),
+			clientv3.WithRev(revision))
+	}
 
 	for {
 		select {
