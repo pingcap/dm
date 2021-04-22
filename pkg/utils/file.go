6c4ffa803caf71effd3e9c9d7f8e4d341e16d7ad
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2021-04-22 17:33:54 +0800

    *: only use go.uber.org/atomic as atomic dependency (#1615) (#1623)

--- pkg/utils/file.go
+++ pkg/utils/file.go
@@ -14,8 +14,14 @@
 package utils
 
 import (
+	"io"
+	"io/ioutil"
 	"os"
+	"path"
 
+	"go.uber.org/zap"
+
+	"github.com/pingcap/dm/pkg/log"
 	"github.com/pingcap/dm/pkg/terror"
 )
 
@@ -63,4 +69,28 @@ func GetFileSize(file string) (int64, error) {
 		return 0, terror.ErrGetFileSize.Delegate(err, file)
 	}
 	return stat.Size(), nil
+}
+
+// WriteFileAtomic writes file to temp and atomically move when everything else succeeds.
+func WriteFileAtomic(filename string, data []byte, perm os.FileMode) error {
+	dir, name := path.Dir(filename), path.Base(filename)
+	f, err := ioutil.TempFile(dir, name)
+	if err != nil {
+		return err
+	}
+	n, err := f.Write(data)
+	f.Close()
+	if err == nil && n < len(data) {
+		err = io.ErrShortWrite
+	} else {
+		err = os.Chmod(f.Name(), perm)
+	}
+	if err != nil {
+		err2 := os.Remove(f.Name())
+		log.L().Warn("failed to remove the temporary file",
+			zap.String("filename", f.Name()),
+			zap.Error(err2))
+		return err
+	}
+	return os.Rename(f.Name(), filename)
 }
