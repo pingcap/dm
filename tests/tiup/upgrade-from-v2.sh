df466ffed1a7bd3b0edd0866398d942f1bd0697f
gmhdbjd <gmhdbjd@gmail.com>
2021-04-30 13:52:11 +0800

    fix test

--- tests/tiup/upgrade-from-v2.sh
+++ tests/tiup/upgrade-from-v2.sh
@@ -58,11 +58,12 @@ function migrate_in_v2 {
     run_dmctl_with_retry $CUR_VER "operate-source show" "mysql-replica-01" 1 "mysql-replica-02" 1
     echo "check workers"
     run_dmctl_with_retry $CUR_VER "list-member --worker" "\"stage\": \"bound\"" 2
-    echo "check locks"
-    run_dmctl_with_retry $CUR_VER "show-ddl-locks" "no DDL lock exists" 1
 
     check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
     check_sync_diff $WORK_DIR $CUR/conf/diff_config_optimistic.toml
+
+    echo "check locks"
+    run_dmctl_with_retry $CUR_VER "show-ddl-locks" "no DDL lock exists" 1
 
     tiup dmctl:$CUR_VER --master-addr=master1:8261 stop-task $TASK_NAME
 }
