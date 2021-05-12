90b1fd6477a69e4610099e36a073e08b8f99d815
Ehco <zh19960202@gmail.com>
2021-05-12 21:21:41 +0800

    lint: add shfmt for bash scripts (#1652)

--- tests/dmctl_advance/check_list/unlock_ddl_lock.sh
+++ tests/dmctl_advance/check_list/unlock_ddl_lock.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 function unlock_ddl_lock_wrong_arg() {
-    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
-        "unlock-ddl-lock" \
-        "unlock-ddl-lock <lock-ID> \[flags\]" 1
+	run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+		"unlock-ddl-lock" \
+		"unlock-ddl-lock <lock-ID> \[flags\]" 1
 }
 
 function unlock_ddl_lock_invalid_force_remove() {
-    force_remove_val="invalid-force-remove"
-    run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
-        "unlock-ddl-lock lock_id --force-remove=$force_remove_val" \
-        "Error: invalid argument \"$force_remove_val\" for \"-f, --force-remove\"" 1
+	force_remove_val="invalid-force-remove"
+	run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+		"unlock-ddl-lock lock_id --force-remove=$force_remove_val" \
+		"Error: invalid argument \"$force_remove_val\" for \"-f, --force-remove\"" 1
 }
