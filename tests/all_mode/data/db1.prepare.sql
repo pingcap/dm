a3b02443d957444a4ea14f3f16f94dafe4a65eaf
glorv <glorvs@163.com>
2021-05-13 19:37:39 +0800

    Support migration data from source with different time_zone by default (#1670)

--- tests/all_mode/data/db1.prepare.sql
+++ tests/all_mode/data/db1.prepare.sql
@@ -1,9 +1,14 @@
 drop database if exists `all_mode`;
 create database `all_mode`;
 use `all_mode`;
-create table t1 (id int NOT NULL AUTO_INCREMENT, name varchar(20), PRIMARY KEY (id));
+create table t1 (
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(20),
+    dt datetime,
+    ts timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id));
 -- test ANSI_QUOTES works with quote in string
-insert into t1 (id, name) values (1, 'ar"ya'), (2, 'catelyn');
+insert into t1 (id, name, dt, ts) values (1, 'ar"ya', now(), now()), (2, 'catelyn', '2021-05-11 10:01:05', '2021-05-11 10:01:05');
 
 -- test sql_mode=NO_AUTO_VALUE_ON_ZERO
 insert into t1 (id, name) values (0, 'lalala');
