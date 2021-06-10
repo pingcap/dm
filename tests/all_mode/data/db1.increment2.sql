dcc68423fbd7b984d05820c4ed4c3c22d66ec508
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2021-06-10 15:30:30 +0800

    syncer: add metric replicationLagGauge back (#1709) (#1759)

---
+++ tests/all_mode/data/db1.increment2.sql
@@ -0,0 +1,5 @@
+SET @@SESSION.TIME_ZONE = "+03:00";
+use all_mode;
+insert into t1 (id, name, ts) values (1001, 'zmj1', '2021-05-11 12:01:05');
+insert into t1 (id, name, ts) values (1002, 'zmj2', '2021-05-11 12:01:05');
+insert into t1 (id, name, ts) values (1003, 'zmj3', '2021-05-11 12:01:05');
