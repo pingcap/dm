81b7274d3463aa1c5c6d26e1fb06deb0a5018702
ehco1996 <zh19960202@gmail.com>
2021-07-29 13:55:04 +0800

    fix TransformerSuite

--- relay/relay_test.go
+++ relay/relay_test.go
@@ -448,7 +448,7 @@ func (t *testRelaySuite) TestHandleEvent(c *C) {
 	defer newcancel()
 	newErr := r.handleEvents(newctx, reader2, transformer2, writer2)
 	c.Assert(errors.Cause(newErr), Equals, newctx.Err())
-	c.Assert(r.meta.Dirty(), Equals, true)
+	c.Assert(r.meta.Dirty(), Equals, false)
 
 	ctx2, cancel2 := context.WithTimeout(context.Background(), 10*time.Millisecond)
 	defer cancel2()
