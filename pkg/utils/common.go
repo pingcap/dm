7848dfa9d46288206af49087c54bdc42c79d33ea
lance6716 <lance6716@gmail.com>
2021-08-05 19:24:01 +0800

    implement more dummy method of interface

--- pkg/utils/common.go
+++ pkg/utils/common.go
@@ -267,6 +267,7 @@ type session struct {
 	sessionctx.Context
 	vars   *variable.SessionVars
 	values map[fmt.Stringer]interface{}
+	builtinFunctionUsage map[string]uint32
 
 	mu sync.RWMutex
 }
@@ -291,6 +292,17 @@ func (se *session) Value(key fmt.Stringer) interface{} {
 	return value
 }
 
+// GetInfoSchema implements the sessionctx.Context interface.
+func (se *session) GetInfoSchema() sessionctx.InfoschemaMetaVersion {
+	return nil
+}
+
+// GetBuiltinFunctionUsage implements the sessionctx.Context interface.
+func (se *session) GetBuiltinFunctionUsage() map[string]uint32 {
+	return se.builtinFunctionUsage
+}
+
+
 // UTCSession can be used as a sessionctx.Context, with UTC timezone.
 var UTCSession *session
 
@@ -300,6 +312,7 @@ func init() {
 	vars.StmtCtx.TimeZone = time.UTC
 	UTCSession.vars = vars
 	UTCSession.values = make(map[fmt.Stringer]interface{}, 1)
+	UTCSession.builtinFunctionUsage = make(map[string]uint32)
 }
 
 // AdjustBinaryProtocolForDatum converts the data in binlog to TiDB datum.
