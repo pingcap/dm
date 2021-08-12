a84c03a74016a8d1a1bdbb906367ec5af026c73d
lance6716 <lance6716@gmail.com>
2021-08-12 11:11:26 +0800

    election: always clean old election key

--- tests/ha_master/run.sh
+++ tests/ha_master/run.sh
@@ -58,14 +58,8 @@ function test_evict_leader() {
 		if [ $i = 4 ]; then
 			continue
 		fi
-		# Leader evict is not effective immediately, we need to wait for a proper period of time.
-		for _ in {0..30}; do
-			NEW_LEADER_NAME=$(get_leader "$WORK_DIR" 127.0.0.1:${MASTER_PORT1})
-			if [ "$NEW_LEADER_NAME" != "$LEADER_NAME" ]; then
-				break
-			fi
-			sleep 1
-		done
+		NEW_LEADER_NAME=$(get_leader $WORK_DIR 127.0.0.1:${MASTER_PORT1})
+    echo "new leader is $NEW_LEADER_NAME"
 		if [ "$NEW_LEADER_NAME" = "$LEADER_NAME" ]; then
 			echo "leader evict failed"
 			exit 1
