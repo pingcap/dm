a9f55e6e414ec43a726e09c61736bd8df75da15c
jerrylisl <562864063@qq.com>
2021-08-18 18:59:00 +0800

    dmctl: move config err

--- dm/ctl/ctl.go
+++ dm/ctl/ctl.go
@@ -215,7 +215,7 @@ func MainStart(args []string) {
 		cfg := common.NewConfig(cmd.Flags())
 		err := cfg.Adjust()
 		if err != nil {
-			return errors.Annotatef(err, "the current command parameter: --config is invalid or used incorrectly")
+			return err
 		}
 
 		err = cfg.Validate()
