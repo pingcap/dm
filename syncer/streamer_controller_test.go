24da6dd7d5b6c9b3b4ffe475b7a357c3e57c42da
glorv <glorvs@163.com>
2021-08-18 15:46:47 +0800

    add retry for binlog event streamer

--- syncer/streamer_controller_test.go
+++ syncer/streamer_controller_test.go
@@ -1,8 +1,10 @@
 package syncer
 
 import (
+	"github.com/go-mysql-org/go-mysql/replication"
 	. "github.com/pingcap/check"
 	"github.com/pingcap/errors"
+	"time"
 )
 
 func (s *testSyncerSuite) TestIsConnectionRefusedError(c *C) {
@@ -14,4 +16,31 @@ func (s *testSyncerSuite) TestIsConnectionRefusedError(c *C) {
 
 	isConnRefusedErr = isConnectionRefusedError(errors.New("connect: connection refused"))
 	c.Assert(isConnRefusedErr, Equals, true)
+}
+
+func (s *testSyncerSuite) TestCanErrorRetry(c *C) {
+	controller := NewStreamerController(replication.BinlogSyncerConfig{}, true, nil,
+		LocalBinlog, "", nil)
+
+	mockErr := errors.New("test")
+
+	// local binlog puller can always retry
+	for i := 0; i < 5; i++ {
+		c.Assert(controller.CanRetry(mockErr), IsTrue)
+	}
+
+	origCfg := minErrorRetryInterval
+	minErrorRetryInterval = 100 * time.Millisecond
+	defer func() {
+		minErrorRetryInterval = origCfg
+	}()
+
+	// test with remote binlog
+	controller = NewStreamerController(replication.BinlogSyncerConfig{}, true, nil,
+		RemoteBinlog, "", nil)
+
+	c.Assert(controller.CanRetry(mockErr), IsTrue)
+	c.Assert(controller.CanRetry(mockErr), IsFalse)
+	time.Sleep(100 * time.Millisecond)
+	c.Assert(controller.CanRetry(mockErr), IsTrue)
 }
