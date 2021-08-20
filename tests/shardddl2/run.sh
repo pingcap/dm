b8295bc5e77e653ebeb07bc0012a3dace8ff059f
lance6716 <lance6716@gmail.com>
2021-08-20 10:06:05 +0800

    swap some cases

--- tests/shardddl2/run.sh
+++ tests/shardddl2/run.sh
@@ -182,152 +182,6 @@ function DM_043() {
 	run_case 043 "double-source-optimistic" "init_table 111 211 212" "clean_table" "optimistic"
 }
 
-function DM_ADD_DROP_COLUMNS_CASE {
-	# add cols
-	run_sql_source1 "alter table ${shardddl1}.${tb1} add column col1 int, add column col2 int, add column col3 int;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(1,now(),1,1,1);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(2,now());"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(3,now());"
-	run_sql_source2 "alter table ${shardddl1}.${tb1} add column col1 int, add column col2 int, add column col3 int;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(4,now(),4,4,4);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(5,now(),5,5,5);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(6,now());"
-	run_sql_source2 "alter table ${shardddl1}.${tb2} add column col1 int, add column col2 int, add column col3 int;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(7,now(),7,7,7);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(8,now(),8,8,8);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(9,now(),9,9,9);"
-
-	# drop cols
-	run_sql_source1 "alter table ${shardddl1}.${tb1} drop column col1, drop column col2;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(11,now(),11);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(12,now(),12,12,12);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(13,now(),13,13,13);"
-	run_sql_source2 "alter table ${shardddl1}.${tb1} drop column col1, drop column col2;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(14,now(),14);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(15,now(),15);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(16,now(),16,16,16);"
-	run_sql_source2 "alter table ${shardddl1}.${tb2} drop column col1, drop column col2;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(17,now(),17);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(18,now(),18);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(19,now(),19);"
-
-	# add and drop
-	run_sql_source1 "alter table ${shardddl1}.${tb1} add column col4 int, drop column col3;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(21,now(),21);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(22,now(),22);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(23,now(),23);"
-	run_sql_source2 "alter table ${shardddl1}.${tb1} add column col4 int, drop column col3;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(24,now(),24);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(25,now(),25);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(26,now(),26);"
-	run_sql_source2 "alter table ${shardddl1}.${tb2} add column col4 int, drop column col3;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(27,now(),27);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(28,now(),28);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(29,now(),29);"
-
-	# drop and add
-	run_sql_source1 "alter table ${shardddl1}.${tb1} drop column col4, add column col5 int;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(31,now(),31);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(32,now(),32);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(33,now(),33);"
-	run_sql_source2 "alter table ${shardddl1}.${tb1} drop column col4, add column col5 int;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(34,now(),34);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(35,now(),35);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(36,now(),36);"
-	run_sql_source2 "alter table ${shardddl1}.${tb2} drop column col4, add column col5 int;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(37,now(),37);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(38,now(),38);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(39,now(),39);"
-
-	check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
-}
-
-function DM_ADD_DROP_COLUMNS() {
-	run_case ADD_DROP_COLUMNS "double-source-pessimistic" \
-		"run_sql_source1 \"create table ${shardddl1}.${tb1} (a int primary key, id datetime);\"; \
-     run_sql_source2 \"create table ${shardddl1}.${tb1} (a int primary key, id datetime);\"; \
-     run_sql_source2 \"create table ${shardddl1}.${tb2} (a int primary key, id datetime);\"" \
-		"clean_table" "pessimistic"
-	run_case ADD_DROP_COLUMNS "double-source-optimistic" \
-		"run_sql_source1 \"create table ${shardddl1}.${tb1} (a int primary key, id datetime);\"; \
-     run_sql_source2 \"create table ${shardddl1}.${tb1} (a int primary key, id datetime);\"; \
-     run_sql_source2 \"create table ${shardddl1}.${tb2} (a int primary key, id datetime);\"" \
-		"clean_table" "optimistic"
-}
-
-function DM_COLUMN_INDEX_CASE {
-	# add col and index
-	run_sql_source1 "alter table ${shardddl1}.${tb1} add column col3 int, add index idx_col1(col1);"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(1,1,1,1);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(2,2,2);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(3,3,3);"
-	run_sql_source2 "alter table ${shardddl1}.${tb1} add column col3 int, add index idx_col1(col1);"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(4,4,4,4);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(5,5,5,5);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(6,6,6);"
-	run_sql_source2 "alter table ${shardddl1}.${tb2} add column col3 int, add index idx_col1(col1);"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(7,7,7,7);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(8,8,8,8);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(9,9,9,9);"
-
-	# drop col and index
-	run_sql_source1 "alter table ${shardddl1}.${tb1} drop column col2, drop index idx_col1;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(11,11,11);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(12,12,12,12);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(13,13,13,13);"
-	run_sql_source2 "alter table ${shardddl1}.${tb1} drop column col2, drop index idx_col1;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(14,14,14);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(15,15,15);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(16,16,16,16);"
-	run_sql_source2 "alter table ${shardddl1}.${tb2} drop column col2, drop index idx_col1;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(17,17,17);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(18,18,18);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(19,19,19);"
-
-	# drop col, add index
-	run_sql_source1 "alter table ${shardddl1}.${tb1} drop column col1, add index idx_col3(col3);"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(21,21);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(22,22,22);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(23,23,23);"
-	run_sql_source2 "alter table ${shardddl1}.${tb1} drop column col1, add index idx_col3(col3);"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(24,24);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(25,25);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(26,26,26);"
-	run_sql_source2 "alter table ${shardddl1}.${tb2} drop column col1, add index idx_col3(col3);"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(27,27);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(28,28);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(29,29);"
-
-	# add col, drop index
-	run_sql_source1 "alter table ${shardddl1}.${tb1} add column col4 int, drop index idx_col3;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(31,31,31);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(32,32);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(33,33);"
-	run_sql_source2 "alter table ${shardddl1}.${tb1} add column col4 int, drop index idx_col3;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(34,34,34);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(35,35,35);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(36,36);"
-	run_sql_source2 "alter table ${shardddl1}.${tb2} add column col4 int, drop index idx_col3;"
-	run_sql_source1 "insert into ${shardddl1}.${tb1} values(37,37,37);"
-	run_sql_source2 "insert into ${shardddl1}.${tb1} values(38,38,38);"
-	run_sql_source2 "insert into ${shardddl1}.${tb2} values(39,39,39);"
-
-	check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
-}
-
-function DM_COLUMN_INDEX() {
-	run_case COLUMN_INDEX "double-source-pessimistic" \
-		"run_sql_source1 \"create table ${shardddl1}.${tb1} (a int primary key, col1 int, col2 int);\"; \
-     run_sql_source2 \"create table ${shardddl1}.${tb1} (a int primary key, col1 int, col2 int);\"; \
-     run_sql_source2 \"create table ${shardddl1}.${tb2} (a int primary key, col1 int, col2 int);\"" \
-		"clean_table" "pessimistic"
-	run_case COLUMN_INDEX "double-source-optimistic" \
-		"run_sql_source1 \"create table ${shardddl1}.${tb1} (a int primary key, col1 int, col2 int);\"; \
-     run_sql_source2 \"create table ${shardddl1}.${tb1} (a int primary key, col1 int, col2 int);\"; \
-     run_sql_source2 \"create table ${shardddl1}.${tb2} (a int primary key, col1 int, col2 int);\"" \
-		"clean_table" "optimistic"
-}
-
 function DM_INIT_SCHEMA_CASE() {
 	run_sql_source1 "insert into ${shardddl1}.${tb1} values(1);"
 	run_sql_source2 "insert into ${shardddl1}.${tb1} values(2);"
@@ -508,14 +362,167 @@ function DM_DROP_COLUMN_ALL_DONE() {
 		"clean_table" "optimistic"
 }
 
+function DM_RECOVER_LOCK_CASE() {
+	# tb1(a,b) tb2(a,b)
+	run_sql_source1 "insert into ${shardddl1}.${tb1} values(1,1);"
+	run_sql_source2 "insert into ${shardddl1}.${tb2} values(2,2);"
+
+	# tb1(a,b,c); tb2(a,b)
+	run_sql_source1 "alter table ${shardddl1}.${tb1} add column c varchar(10);"
+	run_sql_source1 "insert into ${shardddl1}.${tb1} values(3,3,'aaa');"
+	check_log_contain_with_retry "putted a shard DDL.*tb1.*ALTER TABLE .* ADD COLUMN" $WORK_DIR/worker1/log/dm-worker.log $WORK_DIR/worker2/log/dm-worker.log
+
+	# tb1(a,b,c); tb2(a)
+	run_sql_source2 "alter table ${shardddl1}.${tb2} drop column b;"
+	check_log_contain_with_retry "putted a shard DDL.*tb2.*ALTER TABLE .* DROP COLUMN" $WORK_DIR/worker1/log/dm-worker.log $WORK_DIR/worker2/log/dm-worker.log
+
+	restart_master
+
+	run_sql_source1 "alter table ${shardddl1}.${tb1} drop column b;"
+	run_sql_source1 "insert into ${shardddl1}.${tb1} values(4,'bbb');"
+	run_sql_source2 "insert into ${shardddl1}.${tb2} values(5);"
+	check_log_contain_with_retry "putted a shard DDL.*tb1.*ALTER TABLE .* DROP COLUMN" $WORK_DIR/worker1/log/dm-worker.log $WORK_DIR/worker2/log/dm-worker.log
+
+	# tb1(a,c); tb2(a,b)
+	run_sql_source2 "alter table ${shardddl1}.${tb2} add column b int;"
+	run_sql_source1 "insert into ${shardddl1}.${tb1} values(6,'ccc');"
+	run_sql_source2 "insert into ${shardddl1}.${tb2} values(7,7);"
+	check_log_contain_with_retry "putted a shard DDL.*tb2.*ALTER TABLE .* ADD COLUMN" $WORK_DIR/worker1/log/dm-worker.log $WORK_DIR/worker2/log/dm-worker.log
+
+	# recover lock, tb1's info: (a,b,c)->(a,c); tb2's info: (a)->(a,b)
+	# joined(a,b,c); tb1(a,b,c); tb2(a)
+	# TrySync tb1: joined(a,b,c); tb1(a,c); tb2(a)
+	# TrySync tb2: joined(a,c); tb1(a,c); tb2(a,b)
+	restart_master
+
+	# because we disabled auto-resume feature to satisfy other tests, we manually resume once to handle task was paused by
+	# errors of master restarting
+	run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+		"resume-task test" \
+		"\"result\": true" 3
+
+	run_sql_source1 "insert into ${shardddl1}.${tb1} values(8,'eee');"
+	run_sql_source2 "insert into ${shardddl1}.${tb2} values(9,9);"
+
+	run_sql_source1 "alter table ${shardddl1}.${tb1} add column b int;"
+	run_sql_source2 "alter table ${shardddl1}.${tb2} add column c varchar(10) after a;"
+	run_sql_source1 "insert into ${shardddl1}.${tb1} values(10,'fff',10);"
+	run_sql_source2 "insert into ${shardddl1}.${tb2} values(11,'ggg',11);"
+
+	check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
+	run_dm_ctl_with_retry $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+		"shard-ddl-lock" \
+		"no DDL lock exists" 1
+}
+
+function DM_RECOVER_LOCK() {
+	run_case RECOVER_LOCK "double-source-optimistic" \
+		"run_sql_source1 \"create table ${shardddl1}.${tb1} (a int primary key, b int) DEFAULT CHARSET=latin1;\"; \
+     run_sql_source2 \"create table ${shardddl1}.${tb2} (a int primary key, b int) DEFAULT CHARSET=latin1;\"" \
+		"clean_table" "optimistic"
+}
+
+function restart_master_on_pos() {
+	if [ "$1" = "$2" ]; then
+		restart_master
+	fi
+}
+
+function DM_DropAddColumn_CASE() {
+	reset=$2
+
+	run_sql_source1 "insert into ${shardddl1}.${tb1} values(1,1,1);"
+	run_sql_source2 "insert into ${shardddl1}.${tb1} values(2,2,2);"
+
+	check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
+
+	run_sql_source1 "alter table ${shardddl1}.${tb1} drop column c;"
+	run_sql_source1 "insert into ${shardddl1}.${tb1} values(3,3);"
+
+	restart_master_on_pos $reset "1"
+
+	run_sql_source2 "alter table ${shardddl1}.${tb1} drop column c;"
+	run_sql_source2 "insert into ${shardddl1}.${tb1} values(4,4);"
+
+	restart_master_on_pos $reset "2"
+
+	# make sure column c is fully dropped in the downstream
+	check_log_contain_with_retry 'finish to handle ddls in optimistic shard mode' $WORK_DIR/worker1/log/dm-worker.log
+	check_log_contain_with_retry 'finish to handle ddls in optimistic shard mode' $WORK_DIR/worker2/log/dm-worker.log
+
+	run_dm_ctl_with_retry $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+		"shard-ddl-lock" \
+		"no DDL lock exists" 1
+
+	run_sql_source1 "alter table ${shardddl1}.${tb1} drop column b;"
+	run_sql_source1 "insert into ${shardddl1}.${tb1} values(5);"
+
+	restart_master_on_pos $reset "3"
+
+	run_sql_source1 "alter table ${shardddl1}.${tb1} add column c int;"
+	run_sql_source1 "insert into ${shardddl1}.${tb1} values(6,6);"
+
+	restart_master_on_pos $reset "4"
+
+	# make sure task to step in "Sync" stage
+	run_dm_ctl_with_retry $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+		"query-status test" \
+		"\"stage\": \"Running\"" 2 \
+		"\"unit\": \"Sync\"" 2
+	run_sql_source1 "alter table ${shardddl1}.${tb1} add column b int after a;"
+
+	restart_master_on_pos $reset "5"
+
+	run_dm_ctl_with_retry $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+		"query-status test" \
+		"because schema conflict detected" 1 \
+		"add column b that wasn't fully dropped in downstream" 1
+
+	check_sync_diff $WORK_DIR $cur/conf/diff_config.toml 3 'fail'
+	# no ddl error but have un-synced ddl
+	check_metric_not_contains $MASTER_PORT "dm_master_shard_ddl_error" 3
+	# 9223372036854775807 is 2**63 -1
+	check_metric $MASTER_PORT 'dm_master_ddl_state_number{task="test",type="Un-synced"}' 3 0 9223372036854775807
+	# try to fix data
+	echo 'CREATE TABLE `tb1` ( `a` int(11) NOT NULL, `b` int(11) DEFAULT NULL, `c` int(11) DEFAULT NULL, PRIMARY KEY (`a`) /*T![clustered_index] NONCLUSTERED */) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_bin' >${WORK_DIR}/schema.sql
+	run_dm_ctl_with_retry $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+		"binlog-schema update test ${shardddl1} ${tb1} ${WORK_DIR}/schema.sql -s mysql-replica-01" \
+		"\"result\": true" 2
+
+	# skip this error
+	run_dm_ctl_with_retry $WORK_DIR "127.0.0.1:$MASTER_PORT" \
+		"binlog skip test" \
+		"\"result\": true" 2 \
+		"\"source 'mysql-replica-02' has no error\"" 1
+
+	run_sql_source1 "update ${shardddl1}.${tb1} set b=1 where a=1;"
+	run_sql_source1 "update ${shardddl1}.${tb1} set b=3 where a=3;"
+	run_sql_source1 "update ${shardddl1}.${tb1} set b=4 where a=4;"
+	run_sql_source1 "update ${shardddl1}.${tb1} set b=6 where a=6;"
+	run_sql_source2 "alter table ${shardddl1}.${tb1} add column c int"
+	run_sql_source2 "insert into ${shardddl1}.${tb1} values(7,7,7);"
+
+	check_sync_diff $WORK_DIR $cur/conf/diff_config.toml
+}
+
+function DM_DropAddColumn() {
+	for i in $(seq 0 5); do
+		echo "run DM_DropAddColumn case #${i}"
+		run_case DropAddColumn "double-source-optimistic" \
+			"run_sql_source1 \"create table ${shardddl1}.${tb1} (a int primary key, b int, c int);\"; \
+            run_sql_source2 \"create table ${shardddl1}.${tb1} (a int primary key, b int, c int);\"" \
+			"clean_table" "optimistic" "$i"
+	done
+}
+
 function run() {
 	init_cluster
 	init_database
 	DM_DROP_COLUMN_EXEC_ERROR
-	DM_ADD_DROP_COLUMNS
-	DM_COLUMN_INDEX
 	DM_INIT_SCHEMA
 	DM_DROP_COLUMN_ALL_DONE
+	DM_RemoveLock
+	DM_DropAddColumn
 	start=36
 	end=45
 	except=(042 044 045)
