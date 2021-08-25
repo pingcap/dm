3971a4dd2283c1facff14d208b0dcaff0ea9927e
Ehco1996 <zh19960202@gmail.com>
2021-08-25 13:38:04 +0800

    address comment and add test

--- syncer/dml.go
+++ syncer/dml.go
@@ -421,7 +421,7 @@ func genKeyList(table string, columns []*model.ColumnInfo, dataSeq []interface{}
 		}
 		// one column key looks like:`column_val.column_name`
 		buf.WriteString(columnValue(data, &columns[i].FieldType))
-		buf.WriteString(fmt.Sprintf(".%s.", columns[i].Name.String()))
+		buf.WriteString("." + columns[i].Name.String() + ".")
 	}
 	if buf.Len() == 0 {
 		log.L().Debug("all value are nil, no key generated", zap.String("table", table))
