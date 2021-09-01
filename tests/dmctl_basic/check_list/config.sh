0e1b62f20a265c3136bbbbec00b083cf2c943b54
Ehco <zh19960202@gmail.com>
2021-09-02 07:12:14 +0800

    openapi: add source releated API (#2055)

--- tests/dmctl_basic/check_list/config.sh
+++ tests/dmctl_basic/check_list/config.sh
@@ -76,12 +76,12 @@ function diff_get_config() {
 	run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
 		"config source mysql-replica-01 --path $WORK_DIR/get_source1.yaml" \
 		"\"result\": true" 1
-	diff $WORK_DIR/get_source1.yaml $cur/conf/get_source1.yaml || exit 1
+	diff -I '^case-sensitive*' $WORK_DIR/get_source1.yaml $cur/conf/get_source1.yaml || exit 1
 
 	run_dm_ctl $WORK_DIR "127.0.0.1:$MASTER_PORT" \
 		"config source mysql-replica-02 --path $WORK_DIR/get_source2.yaml" \
 		"\"result\": true" 1
-	diff $WORK_DIR/get_source2.yaml $cur/conf/get_source2.yaml || exit 1
+	diff -I '^case-sensitive*' $WORK_DIR/get_source2.yaml $cur/conf/get_source2.yaml || exit 1
 }
 
 function config_to_file() {
