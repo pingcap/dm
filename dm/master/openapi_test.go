47acdc4383c353593e4fa2973d21a5e2bdde4f97
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2021-10-15 07:43:26 +0800

    config: refactor task config (#2169) (#2224)

--- dm/master/openapi_test.go
+++ dm/master/openapi_test.go
@@ -26,7 +26,6 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/pingcap/check"
 	"github.com/pingcap/failpoint"
-	filter "github.com/pingcap/tidb-tools/pkg/binlog-filter"
 	"github.com/tikv/pd/pkg/tempurl"
 	"go.etcd.io/etcd/clientv3"
 	"go.etcd.io/etcd/integration"
@@ -37,6 +36,7 @@ import (
 	"github.com/pingcap/dm/dm/pb"
 	"github.com/pingcap/dm/dm/pbmock"
 	"github.com/pingcap/dm/openapi"
+	"github.com/pingcap/dm/openapi/fixtures"
 	"github.com/pingcap/dm/pkg/ha"
 	"github.com/pingcap/dm/pkg/terror"
 	"github.com/pingcap/dm/pkg/utils"
@@ -44,48 +44,9 @@ import (
 
 var openAPITestSuite = check.SerialSuites(&openAPISuite{})
 
-const (
-	sourceSampleFile = "source.yaml"
-)
-
 // some data for test.
 var (
 	source1Name = "mysql-replica-01"
-	source2Name = "mysql-replica-02"
-
-	taskName   = "test"
-	metaSchema = "dm_meta"
-
-	exportThreads = 4
-	importThreads = 16
-	dataDir       = "./exported_data"
-
-	replBatch   = 200
-	replThreads = 32
-
-	noShardSourceSchema = "some_db"
-	noShardSourceTable  = "*"
-	noShardTargetSchema = "new_name_db"
-	noShardTargetTable  = "*"
-
-	shardSource1BinlogName = "mysql-bin.001"
-	shardSource1BinlogPos  = 0
-	shardSource1GtidSet    = ""
-	shardSource2BinlogName = "mysql-bin.002"
-	shardSource2BinlogPos  = 1232
-	shardSource2GtidSet    = "12e57f06-f360-11eb-8235-585cc2bc66c9:1-24"
-
-	shardSource1Schema = "db_*"
-	shardSource1Table  = "tbl_1"
-	shardSource2Schema = "db_*"
-	shardSource2Table  = "tbl_1"
-
-	shardTargetSchema = "db1"
-	shardTargetTable  = "tbl"
-
-	shardSource1FilterName  = "filterA"
-	shardSource1FilterEvent = "drop database"
-	shardSource1FilterSQL   = "^Drop"
 )
 
 type openAPISuite struct {
@@ -418,7 +379,8 @@ func (t *openAPISuite) TestTaskAPI(c *check.C) {
 	// create task
 	taskURL := "/api/v1/tasks"
 
-	task := genNoShardTask()
+	task, err := fixtures.GenNoShardOpenAPITaskForTest()
+	c.Assert(err, check.IsNil)
 	// use a valid target db
 	task.TargetConfig.Host = dbCfg.Host
 	task.TargetConfig.Port = dbCfg.Port
@@ -429,444 +391,19 @@ func (t *openAPISuite) TestTaskAPI(c *check.C) {
 	result2 := testutil.NewRequest().Post(taskURL).WithJsonBody(createTaskReq).Go(t.testT, s.echo)
 	c.Assert(result2.Code(), check.Equals, http.StatusCreated)
 	var createTaskResp openapi.Task
-	err := result2.UnmarshalBodyToObject(&createTaskResp)
+	err = result2.UnmarshalBodyToObject(&createTaskResp)
 	c.Assert(err, check.IsNil)
 	c.Assert(task.Name, check.Equals, createTaskResp.Name)
-	subTaskM := s.scheduler.GetSubTaskCfgsByTask(taskName)
+	subTaskM := s.scheduler.GetSubTaskCfgsByTask(task.Name)
 	c.Assert(len(subTaskM) == 1, check.IsTrue)
-	c.Assert(subTaskM[source1Name].Name, check.Equals, taskName)
+	c.Assert(subTaskM[source1Name].Name, check.Equals, task.Name)
 
 	// stop task
 	result3 := testutil.NewRequest().Delete(fmt.Sprintf("%s/%s", taskURL, task.Name)).Go(t.testT, s.echo)
 	c.Assert(result3.Code(), check.Equals, http.StatusNoContent)
-	subTaskM = s.scheduler.GetSubTaskCfgsByTask(taskName)
+	subTaskM = s.scheduler.GetSubTaskCfgsByTask(task.Name)
 	c.Assert(len(subTaskM) == 0, check.IsTrue)
 	c.Assert(failpoint.Disable("github.com/pingcap/dm/dm/master/MockSkipAdjustTargetDB"), check.IsNil)
-}
-
-func (t *openAPISuite) TestModelToSubTaskConfigList(c *check.C) {
-	testNoShardTaskToSubTaskConfig(c)
-	testShardAndFilterTaskToSubTaskConfig(c)
-}
-
-func genNoShardTask() openapi.Task {
-	/* no shard task
-	name: test
-	shard-mode: "pessimistic"
-
-	meta-schema: "dm_meta"
-	enhance-online-schema-change: True
-	on-duplication: error
-
-	target-config:
-	  host: "127.0.0.1"
-	  port: 4000
-	  user: "root"
-	  password: "123456"
-
-	source-config:
-	  full-migrate-conf:
-		export-threads：4
-		import-threads: 16
-		data-dir: "./exported_data"
-	  incr-migrate-conf:
-		repl-threads：32
-		repl-batch: 200
-	  source:
-	  - source-name: "mysql-replica-01"
-	    binlog-name: ""
-	    binlog-pos: 0
-	    gtid: ""
-
-	table-migrate-rule:
-	  - source:
-	       source-name: "mysql-replica-01"
-	       schema: "some_db"
-	       table: "*"
-	*/
-	taskSourceConf := openapi.TaskSourceConf{
-		SourceName: source1Name,
-	}
-	tableMigrateRule := openapi.TaskTableMigrateRule{
-		Source: struct {
-			Schema     string "json:\"schema\""
-			SourceName string "json:\"source_name\""
-			Table      string `json:"table"`
-		}{
-			Schema:     noShardSourceSchema,
-			SourceName: source1Name,
-			Table:      noShardSourceTable,
-		},
-		Target: &struct {
-			Schema string `json:"schema"`
-			Table  string `json:"table"`
-		}{
-			Schema: noShardTargetSchema,
-			Table:  noShardTargetTable,
-		},
-	}
-
-	return openapi.Task{
-		EnhanceOnlineSchemaChange: true,
-		ShardMode:                 nil,
-		Name:                      taskName,
-		MetaSchema:                &metaSchema,
-		TaskMode:                  openapi.TaskTaskModeAll,
-		OnDuplicate:               openapi.TaskOnDuplicateError,
-		TargetConfig: openapi.TaskTargetDataBase{
-			Host:     "root",
-			Password: "123456",
-			Port:     4000,
-			User:     "root",
-		},
-		SourceConfig: openapi.TaskSourceConfig{
-			FullMigrateConf: &openapi.TaskFullMigrateConf{
-				DataDir:       &dataDir,
-				ExportThreads: &exportThreads,
-				ImportThreads: &importThreads,
-			},
-			IncrMigrateConf: &openapi.TaskIncrMigrateConf{
-				ReplBatch:   &replBatch,
-				ReplThreads: &replThreads,
-			},
-			SourceConf: []openapi.TaskSourceConf{taskSourceConf},
-		},
-		TableMigrateRule: []openapi.TaskTableMigrateRule{tableMigrateRule},
-	}
-}
-
-func genShardAndFilterTask() openapi.Task {
-	/* shard and filter task
-	name: test
-	meta-schema: "dm_meta"
-	enhance-online-schema-change: True
-	on-duplication: error
-
-	target-config:
-	  host: "127.0.0.1"
-	  port: 4000
-	  user: "root"
-	  password: "123456"
-
-	source-config:
-	  full-migrate-conf:
-		export-threads：4
-		import-threads: 16
-		data-dir: "./exported_data"
-	  incr-migrate-conf:
-		repl-threads：32
-		repl-batch: 200
-	  source:
-	  - source-name: "mysql-replica-01"
-	    binlog-name: "mysql-bin.001"
-	    binlog-pos: 0
-	    gtid: ""
-	  - source-name: "mysql-replica-02"
-	    binlog-name: "mysql-bin.002"
-	    binlog-pos: 1232
-	    gtid: "12e57f06-f360-11eb-8235-585cc2bc66c9:1-24"
-
-	table-migrate-rule:
-	  - source:
-	       source-name: "mysql-replica-01"
-	       schema: "db_*"
-	       table: "tbl_1"
-	    target:
-	       schema: "db1"
-	       table: "tbl"
-	    binlog-filter-rule: ["filterA"]
-	  - source:
-	       source-name: "mysql-replica-02"
-	       schema: "db_*"
-	       table: "tbl_1"
-	    target:
-	       schema: "db1"
-	       table: "tbl"
-
-	binlog-filter—rule:
-	   -name: "filterA"
-	    ignore-event: ["drop database"]
-	    ignore-sql: ["^Drop"]
-	   -name: "filterB"
-	    ignore-event: ["drop database"]
-	    ignore-sql: ["^Create"]
-	*/
-	taskSource1Conf := openapi.TaskSourceConf{
-		BinlogGtid: &shardSource1GtidSet,
-		BinlogName: &shardSource1BinlogName,
-		BinlogPos:  &shardSource1BinlogPos,
-		SourceName: source1Name,
-	}
-	taskSource2Conf := openapi.TaskSourceConf{
-		SourceName: source2Name,
-		BinlogGtid: &shardSource2GtidSet,
-		BinlogName: &shardSource2BinlogName,
-		BinlogPos:  &shardSource2BinlogPos,
-	}
-
-	binlogFilterNameList := []string{shardSource1FilterName}
-	ignoreEvent := []string{shardSource1FilterEvent}
-	ignoreSQL := []string{shardSource1FilterSQL}
-	eventFilterRule := openapi.TaskBinLogFilterRule{IgnoreEvent: &ignoreEvent, IgnoreSql: &ignoreSQL}
-	binlogFilterRuleMap := openapi.Task_BinlogFilterRule{}
-	binlogFilterRuleMap.Set(shardSource1FilterName, eventFilterRule)
-
-	tableMigrateRule1 := openapi.TaskTableMigrateRule{
-		BinlogFilterRule: &binlogFilterNameList,
-		Source: struct {
-			Schema     string "json:\"schema\""
-			SourceName string "json:\"source_name\""
-			Table      string `json:"table"`
-		}{
-			Schema:     shardSource1Schema,
-			SourceName: source1Name,
-			Table:      shardSource1Table,
-		},
-		Target: &struct {
-			Schema string `json:"schema"`
-			Table  string `json:"table"`
-		}{
-			Schema: shardTargetSchema,
-			Table:  shardTargetTable,
-		},
-	}
-	tableMigrateRule2 := openapi.TaskTableMigrateRule{
-		Source: struct {
-			Schema     string "json:\"schema\""
-			SourceName string "json:\"source_name\""
-			Table      string `json:"table"`
-		}{
-			Schema:     shardSource2Schema,
-			SourceName: source2Name,
-			Table:      shardSource2Table,
-		},
-		Target: &struct {
-			Schema string `json:"schema"`
-			Table  string `json:"table"`
-		}{
-			Schema: shardTargetSchema,
-			Table:  shardTargetTable,
-		},
-	}
-	shardMode := openapi.TaskShardModePessimistic
-	return openapi.Task{
-		EnhanceOnlineSchemaChange: true,
-		ShardMode:                 &shardMode,
-		Name:                      taskName,
-		MetaSchema:                &metaSchema,
-		TaskMode:                  openapi.TaskTaskModeAll,
-		OnDuplicate:               openapi.TaskOnDuplicateError,
-		BinlogFilterRule:          &binlogFilterRuleMap,
-		TargetConfig: openapi.TaskTargetDataBase{
-			Host:     "root",
-			Password: "123456",
-			Port:     4000,
-			User:     "root",
-		},
-		SourceConfig: openapi.TaskSourceConfig{
-			FullMigrateConf: &openapi.TaskFullMigrateConf{
-				DataDir:       &dataDir,
-				ExportThreads: &exportThreads,
-				ImportThreads: &importThreads,
-			},
-			IncrMigrateConf: &openapi.TaskIncrMigrateConf{
-				ReplBatch:   &replBatch,
-				ReplThreads: &replThreads,
-			},
-			SourceConf: []openapi.TaskSourceConf{taskSource1Conf, taskSource2Conf},
-		},
-		TableMigrateRule: []openapi.TaskTableMigrateRule{tableMigrateRule1, tableMigrateRule2},
-	}
-}
-
-func testNoShardTaskToSubTaskConfig(c *check.C) {
-	ctx, cancel := context.WithCancel(context.Background())
-	s := setupServer(ctx, c)
-	defer func() {
-		cancel()
-		s.Close()
-	}()
-
-	sourceCfg1, err := config.LoadFromFile(sourceSampleFile)
-	c.Assert(err, check.IsNil)
-	sourceCfg1.SourceID = source1Name
-	c.Assert(s.scheduler.AddSourceCfg(sourceCfg1), check.IsNil)
-	task := genNoShardTask()
-	toDBCfg := &config.DBConfig{
-		Host:     task.TargetConfig.Host,
-		Port:     task.TargetConfig.Port,
-		User:     task.TargetConfig.User,
-		Password: task.TargetConfig.Password,
-	}
-
-	subTaskConfigList, err := s.modelToSubTaskConfigList(toDBCfg, &task)
-	c.Assert(err, check.IsNil)
-	c.Assert(subTaskConfigList, check.HasLen, 1)
-	subTaskConfig := subTaskConfigList[0]
-	// check task name and mode
-	c.Assert(subTaskConfig.Name, check.Equals, taskName)
-	// check task meta
-	c.Assert(subTaskConfig.MetaSchema, check.Equals, metaSchema)
-	c.Assert(subTaskConfig.Meta, check.IsNil)
-	// check shard config
-	c.Assert(subTaskConfig.ShardMode, check.Equals, "")
-	// check online schema change
-	c.Assert(subTaskConfig.OnlineDDL, check.Equals, true)
-	// check case sensitive
-	c.Assert(subTaskConfig.CaseSensitive, check.Equals, sourceCfg1.CaseSensitive)
-	// check from
-	c.Assert(subTaskConfig.From.Host, check.Equals, sourceCfg1.From.Host)
-	// check to
-	c.Assert(subTaskConfig.To.Host, check.Equals, toDBCfg.Host)
-	// check dumpling loader syncer config
-	c.Assert(subTaskConfig.MydumperConfig.Threads, check.Equals, exportThreads)
-	c.Assert(subTaskConfig.LoaderConfig.Dir, check.Equals, fmt.Sprintf("%s.%s", dataDir, taskName))
-	c.Assert(subTaskConfig.LoaderConfig.PoolSize, check.Equals, importThreads)
-	c.Assert(subTaskConfig.SyncerConfig.WorkerCount, check.Equals, replThreads)
-	c.Assert(subTaskConfig.SyncerConfig.Batch, check.Equals, replBatch)
-	// check route
-	c.Assert(subTaskConfig.RouteRules, check.HasLen, 1)
-	rule := subTaskConfig.RouteRules[0]
-	c.Assert(rule.SchemaPattern, check.Equals, noShardSourceSchema)
-	c.Assert(rule.TablePattern, check.Equals, noShardSourceTable)
-	c.Assert(rule.TargetSchema, check.Equals, noShardTargetSchema)
-	c.Assert(rule.TargetTable, check.Equals, noShardTargetTable)
-	// check filter
-	c.Assert(subTaskConfig.FilterRules, check.HasLen, 0)
-	// check balist
-	c.Assert(subTaskConfig.BAList, check.NotNil)
-	ba := subTaskConfig.BAList
-	c.Assert(ba.DoDBs, check.HasLen, 1)
-	c.Assert(ba.DoDBs[0], check.Equals, noShardSourceSchema)
-	c.Assert(ba.DoTables, check.HasLen, 1)
-	c.Assert(ba.DoTables[0].Name, check.Equals, noShardSourceTable)
-	c.Assert(ba.DoTables[0].Schema, check.Equals, noShardSourceSchema)
-}
-
-func testShardAndFilterTaskToSubTaskConfig(c *check.C) {
-	ctx, cancel := context.WithCancel(context.Background())
-	s := setupServer(ctx, c)
-	defer func() {
-		cancel()
-		s.Close()
-	}()
-
-	sourceCfg1, err := config.LoadFromFile(sourceSampleFile)
-	c.Assert(err, check.IsNil)
-	sourceCfg1.SourceID = source1Name
-	// sourceCfg1.From.m
-	sourceCfg2, err := config.LoadFromFile(sourceSampleFile)
-	c.Assert(err, check.IsNil)
-	sourceCfg2.SourceID = source2Name
-	c.Assert(s.scheduler.AddSourceCfg(sourceCfg1), check.IsNil)
-	c.Assert(s.scheduler.AddSourceCfg(sourceCfg2), check.IsNil)
-
-	task := genShardAndFilterTask()
-	toDBCfg := &config.DBConfig{
-		Host:     task.TargetConfig.Host,
-		Port:     task.TargetConfig.Port,
-		User:     task.TargetConfig.User,
-		Password: task.TargetConfig.Password,
-	}
-
-	subTaskConfigList, err := s.modelToSubTaskConfigList(toDBCfg, &task)
-	c.Assert(err, check.IsNil)
-	c.Assert(subTaskConfigList, check.HasLen, 2)
-
-	// check sub task 1
-	subTask1Config := subTaskConfigList[0]
-	// check task name and mode
-	c.Assert(subTask1Config.Name, check.Equals, taskName)
-	// check task meta
-	c.Assert(subTask1Config.MetaSchema, check.Equals, metaSchema)
-	c.Assert(subTask1Config.Meta, check.NotNil)
-	c.Assert(subTask1Config.Meta.BinLogGTID, check.Equals, shardSource1GtidSet)
-	c.Assert(subTask1Config.Meta.BinLogName, check.Equals, shardSource1BinlogName)
-	c.Assert(subTask1Config.Meta.BinLogPos, check.Equals, uint32(shardSource1BinlogPos))
-	// check shard config
-	c.Assert(subTask1Config.ShardMode, check.Equals, string(openapi.TaskShardModePessimistic))
-	// check online schema change
-	c.Assert(subTask1Config.OnlineDDL, check.Equals, true)
-	// check case sensitive
-	c.Assert(subTask1Config.CaseSensitive, check.Equals, sourceCfg1.CaseSensitive)
-	// check from
-	c.Assert(subTask1Config.From.Host, check.Equals, sourceCfg1.From.Host)
-	// check to
-	c.Assert(subTask1Config.To.Host, check.Equals, toDBCfg.Host)
-	// check dumpling loader syncer config
-	c.Assert(subTask1Config.MydumperConfig.Threads, check.Equals, exportThreads)
-	c.Assert(subTask1Config.LoaderConfig.Dir, check.Equals, fmt.Sprintf("%s.%s", dataDir, taskName))
-	c.Assert(subTask1Config.LoaderConfig.PoolSize, check.Equals, importThreads)
-	c.Assert(subTask1Config.SyncerConfig.WorkerCount, check.Equals, replThreads)
-	c.Assert(subTask1Config.SyncerConfig.Batch, check.Equals, replBatch)
-	// check route
-	c.Assert(subTask1Config.RouteRules, check.HasLen, 1)
-	rule := subTask1Config.RouteRules[0]
-	c.Assert(rule.SchemaPattern, check.Equals, shardSource1Schema)
-	c.Assert(rule.TablePattern, check.Equals, shardSource1Table)
-	c.Assert(rule.TargetSchema, check.Equals, shardTargetSchema)
-	c.Assert(rule.TargetTable, check.Equals, shardTargetTable)
-	// check filter
-	c.Assert(subTask1Config.FilterRules, check.HasLen, 1)
-	c.Assert(subTask1Config.FilterRules[0].SchemaPattern, check.Equals, shardSource1Schema)
-	c.Assert(subTask1Config.FilterRules[0].TablePattern, check.Equals, shardSource1Table)
-	c.Assert(subTask1Config.FilterRules[0].Action, check.Equals, filter.Ignore)
-	c.Assert(subTask1Config.FilterRules[0].SQLPattern, check.HasLen, 1)
-	c.Assert(subTask1Config.FilterRules[0].SQLPattern[0], check.Equals, shardSource1FilterSQL)
-	c.Assert(subTask1Config.FilterRules[0].Events, check.HasLen, 1)
-	c.Assert(string(subTask1Config.FilterRules[0].Events[0]), check.Equals, shardSource1FilterEvent)
-	// check balist
-	c.Assert(subTask1Config.BAList, check.NotNil)
-	ba := subTask1Config.BAList
-	c.Assert(ba.DoDBs, check.HasLen, 1)
-	c.Assert(ba.DoDBs[0], check.Equals, shardSource1Schema)
-	c.Assert(ba.DoTables, check.HasLen, 1)
-	c.Assert(ba.DoTables[0].Name, check.Equals, shardSource1Table)
-	c.Assert(ba.DoTables[0].Schema, check.Equals, shardSource1Schema)
-
-	// check sub task 2
-	subTask2Config := subTaskConfigList[1]
-	// check task name and mode
-	c.Assert(subTask2Config.Name, check.Equals, taskName)
-	// check task meta
-	c.Assert(subTask2Config.MetaSchema, check.Equals, metaSchema)
-	c.Assert(subTask2Config.Meta, check.NotNil)
-	c.Assert(subTask2Config.Meta.BinLogGTID, check.Equals, shardSource2GtidSet)
-	c.Assert(subTask2Config.Meta.BinLogName, check.Equals, shardSource2BinlogName)
-	c.Assert(subTask2Config.Meta.BinLogPos, check.Equals, uint32(shardSource2BinlogPos))
-	// check shard config
-	c.Assert(subTask2Config.ShardMode, check.Equals, string(openapi.TaskShardModePessimistic))
-	// check online schema change
-	c.Assert(subTask2Config.OnlineDDL, check.Equals, true)
-	// check case sensitive
-	c.Assert(subTask2Config.CaseSensitive, check.Equals, sourceCfg2.CaseSensitive)
-	// check from
-	c.Assert(subTask2Config.From.Host, check.Equals, sourceCfg2.From.Host)
-	// check to
-	c.Assert(subTask2Config.To.Host, check.Equals, toDBCfg.Host)
-	// check dumpling loader syncer config
-	c.Assert(subTask2Config.MydumperConfig.Threads, check.Equals, exportThreads)
-	c.Assert(subTask2Config.LoaderConfig.Dir, check.Equals, fmt.Sprintf("%s.%s", dataDir, taskName))
-	c.Assert(subTask2Config.LoaderConfig.PoolSize, check.Equals, importThreads)
-	c.Assert(subTask2Config.SyncerConfig.WorkerCount, check.Equals, replThreads)
-	c.Assert(subTask2Config.SyncerConfig.Batch, check.Equals, replBatch)
-	// check route
-	c.Assert(subTask2Config.RouteRules, check.HasLen, 1)
-	rule = subTask2Config.RouteRules[0]
-	c.Assert(rule.SchemaPattern, check.Equals, shardSource2Schema)
-	c.Assert(rule.TablePattern, check.Equals, shardSource2Table)
-	c.Assert(rule.TargetSchema, check.Equals, shardTargetSchema)
-	c.Assert(rule.TargetTable, check.Equals, shardTargetTable)
-	// check filter
-	c.Assert(subTask2Config.FilterRules, check.HasLen, 0)
-	// check balist
-	c.Assert(subTask1Config.BAList, check.NotNil)
-	ba = subTask2Config.BAList
-	c.Assert(ba.DoDBs, check.HasLen, 1)
-	c.Assert(ba.DoDBs[0], check.Equals, shardSource2Schema)
-	c.Assert(ba.DoTables, check.HasLen, 1)
-	c.Assert(ba.DoTables[0].Name, check.Equals, shardSource2Table)
-	c.Assert(ba.DoTables[0].Schema, check.Equals, shardSource2Schema)
 }
 
 func setupServer(ctx context.Context, c *check.C) *Server {
